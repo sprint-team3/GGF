c1de63be149a38e681bcf463112134821f0dec57
CheeseB <2489ckckck@naver.com>
2024-03-03 14:31:36 +0900

    init: 프로젝트 초기 세팅

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,8 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='ko'>
       <Head />
       <body>
         <Main />
