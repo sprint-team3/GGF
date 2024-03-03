a4c8e4b4ebba54ee9ba0eeabbd8d5635c77a2165
CheeseB <2489ckckck@naver.com>
2024-03-03 13:58:15 +0900

    init: 프로젝트 초기 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@import "@/styles/main.scss";`,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'sprint-fe-project.s3.ap-northeast-2.amazonaws.com',
+        port: '',
+        pathname: '/globalnomad/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
