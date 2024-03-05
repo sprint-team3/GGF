e23a7acfb767f0207f05e382a4a9139ed923069b
grapefruit13 <liam.heo11@gmail.com>
2024-03-05 22:16:35 +0900

    feat: myReservations  api 메서드 추상화 및 타입 정의

---
+++ src/types/myReservations.ts
@@ -0,0 +1,38 @@
+export type MyReservations = {
+  cursorId: number;
+  reservations: MyReservationsReservation[];
+  totalCount: number;
+};
+
+export type MyReservationsReservation = {
+  id: number;
+  teamId: string;
+  userId: number;
+  activity: {
+    bannerImageUrl: string;
+    title: string;
+    id: number;
+  };
+  scheduleId: number;
+  status: MyReservationsStatus;
+  reviewSubmitted: boolean;
+  totalPrice: number;
+  headCount: number;
+  date: string;
+  startTime: string;
+  endTime: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type MyReservationscreateReviewParams = {
+  rating: number;
+  content: string;
+};
+
+export type MyReservationsStatus =
+  | 'pending'
+  | 'confirmed'
+  | 'completed'
+  | 'declined'
+  | 'canceled';
