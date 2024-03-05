35a21a226a4e017153b4fba9399c357be235654d
CheeseB <2489ckckck@naver.com>
2024-03-05 14:31:21 +0900

    feat: activities api 추상화 및 타입 정의

---
+++ src/apis/activities.ts
@@ -0,0 +1,41 @@
+import {
+  ACTIVITIES_API,
+  AVAILABLE_SCHEDULE_API,
+  REVIEWS_API,
+  RESERVATIONS_API,
+  IMAGE_API,
+  PAGE_SIZE,
+  PAGE_METHOD,
+} from '@/constants';
+import { Activity, Reservation } from '@/types';
+
+import instance from './axios';
+
+const Activities = {
+  create: (value: Activity) => instance.post(ACTIVITIES_API, value),
+
+  get: (activityId: number) => instance.get(`${ACTIVITIES_API}/${activityId}`),
+
+  getList: () =>
+    instance.get(ACTIVITIES_API, {
+      params: { size: PAGE_SIZE, method: PAGE_METHOD },
+    }),
+
+  getScheduleList: (activityId: number, year: string, month: string) =>
+    instance.get(`${ACTIVITIES_API}/${activityId}${AVAILABLE_SCHEDULE_API}`, {
+      params: { year, month },
+    }),
+
+  getReviewList: (activityId: number) =>
+    instance.get(`${ACTIVITIES_API}/${activityId}${REVIEWS_API}`, {
+      params: { size: PAGE_SIZE },
+    }),
+
+  createReservation: (activityId: number, value: Reservation) =>
+    instance.post(`${ACTIVITIES_API}/${activityId}${RESERVATIONS_API}`, value),
+
+  createImage: (value: string) =>
+    instance.post(`${ACTIVITIES_API}${IMAGE_API}`, value),
+};
+
+export default Activities;
---
+++ src/constants/paging.ts
@@ -0,0 +1,2 @@
+export const PAGE_SIZE = 1000;
+export const PAGE_METHOD = 'offset';
