dc34b482b13e181eb208e9cc04a4b1e1b97763c1
CheeseB <2489ckckck@naver.com>
2024-03-05 14:31:21 +0900

    feat: activities api 추상화 및 타입 정의

---
+++ src/types/activities.ts
@@ -0,0 +1,21 @@
+export type Activity = {
+  title: string;
+  category: string;
+  description: string;
+  address: string;
+  price: number;
+  schedules: Schedule[];
+  bannerImageUrl: string;
+  subImageUrls: string[];
+};
+
+export type Schedule = {
+  date: string;
+  startTime: string;
+  endTime: string;
+};
+
+export type Reservation = {
+  scheduleId: number;
+  headCount: number;
+};
