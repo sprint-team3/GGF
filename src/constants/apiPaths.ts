f788ab9edda0bade7469ac7646ff9172136467d0
grapefruit13 <liam.heo11@gmail.com>
2024-03-05 13:29:53 +0900

    feat: api 엔드포인트 path constants에 저장

---
+++ src/constants/apiPaths.ts
@@ -0,0 +1,18 @@
+export const ACTIVITIES_API = '/activities';
+export const AVAILABLE_SCHEDULE_API = '/available-schedule';
+export const REVIEWS_API = '/reviews';
+export const RESERVATIONS_API = '/reservations';
+export const IMAGE_API = '/image';
+
+export const AUTH_API = '/auth';
+export const LOGIN_API = '/login';
+export const TOKENS_API = '/tokens';
+
+export const MY_ACTIVITIES_API = '/my-activities';
+export const RESERVATION_DASHBOARD_API = '/reservation-dashboard';
+export const RESERVED_SCHEDULE_API = '/reserved-schedule';
+
+export const MY_NOTIFICATIONS_API = '/my-notifications';
+export const MY_RESERVATIONS_API = '/my-reservations';
+export const USERS_API = '/users';
+export const ME_API = '/me';
