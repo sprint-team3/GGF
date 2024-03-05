2d654b14af5401a08cb63ab76509f4ce4197dd4e
ccwnc <ccwnc337@gmail.com>
2024-03-05 15:49:04 +0900

    fix: instance 대문자에서 소문자로 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,13 +1,14 @@
-import Instance from 'axios';
-
-import { AUTH_API, LOGIN_API, TOKENS_API } from '@/constants/apiPaths';
 import { Acount } from '@/types/auth';
 
+import instance from './axios';
+
+import { AUTH_API, LOGIN_API, TOKENS_API } from '@/constants/apiPaths';
+
 export const Auth = {
-  signin: (value: Acount) => Instance.post(`${AUTH_API}${LOGIN_API}`, value),
+  signin: (value: Acount) => instance.post(`${AUTH_API}${LOGIN_API}`, value),
 
   renewToken: (refreshToken: string) =>
-    Instance.post(`${AUTH_API}${TOKENS_API}`, {
+    instance.post(`${AUTH_API}${TOKENS_API}`, {
       headers: {
         Authorization: `Bearer ${refreshToken}`,
       },
