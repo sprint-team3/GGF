29b89c1429d8c58b27b557998cbbfbf5d685fb8a
ccwnc <ccwnc337@gmail.com>
2024-03-05 19:41:40 +0900

    refactor: headers 삭제 및 params 수정

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,15 +1,10 @@
 import { AUTH_API, LOGIN_API, TOKENS_API } from '@/constants';
-import { Acount } from '@/types';
+import { Account } from '@/types';
 
 import instance from './axios';
 
 export const Auth = {
-  signin: (value: Acount) => instance.post(`${AUTH_API}${LOGIN_API}`, value),
+  signin: (value: Account) => instance.post(`${AUTH_API}${LOGIN_API}`, value),
 
-  renewToken: (refreshToken: string) =>
-    instance.post(`${AUTH_API}${TOKENS_API}`, {
-      headers: {
-        Authorization: `Bearer ${refreshToken}`,
-      },
-    }),
+  renewToken: () => instance.post(`${AUTH_API}${TOKENS_API}`),
 };
