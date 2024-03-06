d8667f50c9960b7b51e99be7754f02d228432ff2
grapefruit13 <liam.heo11@gmail.com>
2024-03-07 00:03:36 +0900

    refactor: axios instance header에 토큰 첨부 로직 구현

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,22 +1,43 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
+import { ACCESS_TOKEN_EXPIRED_TIME, REFRESH_TOKEN_EXPIRED_TIME } from '@/constants';
+
+import { Auth } from './auth';
+
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: false,
 });
 
 instance.interceptors.request.use(
-  (config) => {
+  async (config) => {
     const accessToken = Cookies.get('accessToken');
     const refreshToken = Cookies.get('refreshToken');
+    if (!accessToken && refreshToken) {
+      try {
+        const res = await Auth.renewToken();
+        const { accessToken, refreshToken } = res.data;
+        Cookies.set('accessToken', accessToken, {
+          expires: ACCESS_TOKEN_EXPIRED_TIME,
+          secure: true,
+          sameSite: 'strict',
+        });
+        Cookies.set('refreshToken', refreshToken, {
+          expires: REFRESH_TOKEN_EXPIRED_TIME,
+          secure: true,
+          sameSite: 'strict',
+        });
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
     if (accessToken) {
       config.headers['Authorization'] = `Bearer ${accessToken}`;
-    } else if (!accessToken && refreshToken) {
-      config.headers['Authorization'] = `Bearer ${refreshToken}`;
     }
     return config;
   },
