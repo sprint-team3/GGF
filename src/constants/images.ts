a93c64a3103466f003200d2675ecf8e671dac90d
ccwnc <ccwnc337@gmail.com>
2024-03-07 00:39:44 +0900

    chore: svg constants 추가

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -4,4 +4,40 @@ export const SVGS = {
     url: '/svgs/profile.svg',
     alt: '프로필',
   },
+  profileDefault: {
+    url: '/svgs/profileDefault.svg',
+    alt: '기본 프로필',
+  },
+  profileActive: {
+    url: '/svgs/profileActive.svg',
+    alt: '활성화된 프로필',
+  },
+  arrowDefault: {
+    url: '/svgs/arrowDefault.svg',
+    alt: '기본 화살표',
+  },
+  arrowActive: {
+    url: '/svgs/arrowActive.svg',
+    alt: '활성화된 화살표',
+  },
+  alramDefault: {
+    url: 'svgs/alramDefault.svg',
+    alt: '기본 알람',
+  },
+  alramActive: {
+    url: 'svgs/alramActive.svg',
+    alt: '활성화되고 텅 빈 알람',
+  },
+  alramActive2: {
+    url: 'svgs/alramActive2.svg',
+    alt: '비활성화되고 가득 찬 알람',
+  },
+  alramActive3: {
+    url: 'svgs/alramActive3.svg',
+    alt: '활성화되고 가득 찬 알람',
+  },
+  dog: {
+    url: 'svgs/dog.svg',
+    alt: '개 사진',
+  },
 };
