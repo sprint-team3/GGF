a19caa1ebc836585c3fd02f857dcfaa208fa2f70
grapefruit13 <liam.heo11@gmail.com>
2024-03-06 19:20:31 +0900

    feat: 로그인시 액세스토큰과 리프레시토큰을 쿠키에 저장하는 useSignin 구현

---
+++ src/hooks/useSignin.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import Cookies from 'js-cookie';
+
+import { Auth } from '@/apis/auth';
+import { ACCESS_TOKEN_EXPIRED_TIME, REFRESH_TOKEN_EXPIRED_TIME } from '@/constants';
+
+import { Account } from '@/types';
+
+const useSignin = (value: Account) => {
+  const { data } = useQuery({
+    queryKey: ['login-result'],
+    queryFn: () => Auth.signin(value),
+  });
+
+  if (data) {
+    const { accessToken, refreshToken } = data.data;
+    Cookies.set('accessToken', accessToken, { expires: ACCESS_TOKEN_EXPIRED_TIME, secure: true, sameSite: 'strict' });
+    Cookies.set('refreshToken', refreshToken, {
+      expires: REFRESH_TOKEN_EXPIRED_TIME,
+      secure: true,
+      sameSite: 'strict',
+    });
+  }
+};
+
+export default useSignin;
