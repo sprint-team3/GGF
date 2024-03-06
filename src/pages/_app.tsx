5a9a2a40a43fee5e0308fb3a934037d629da02dc
ccwnc <ccwnc337@gmail.com>
2024-03-07 01:20:28 +0900

    feat: app.tsx에 QueryClient & QueryClientProvider 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import type { AppProps } from 'next/app';
+
 import '@/styles/base/common.scss';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
