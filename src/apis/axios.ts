30557e3f1d1212a0bb28198a74cccdc65ddfb8a8
grapefruit13 <liam.heo11@gmail.com>
2024-03-06 19:21:19 +0900

    feat: axios interceptor로 토큰 header에 첨부

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -7,5 +8,24 @@ const instance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const accessToken = Cookies.get('accessToken');
+    const refreshToken = Cookies.get('refreshToken');
+    if (accessToken) {
+      config.headers['Authorization'] = `Bearer ${accessToken}`;
+    } else if (!accessToken && refreshToken) {
+      config.headers['Authorization'] = `Bearer ${refreshToken}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject(error),
+);
 
 export default instance;
