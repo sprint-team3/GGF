4fe523907d82d9017330ea5fccb4a5768d8a1f45
ccwnc <ccwnc337@gmail.com>
2024-03-07 00:54:37 +0900

    chore: pages 폴더에 test 파일 추가

---
+++ src/pages/test/index.tsx
@@ -0,0 +1,21 @@
+import classNames from 'classnames/bind';
+
+import { Alram } from '@/components/commons/Alram';
+import { Avatar } from '@/components/commons/Avatar';
+import { HeaderProfile } from '@/components/commons/HeaderProfile';
+
+import styles from './Test.module.scss';
+
+const cx = classNames.bind(styles);
+
+const Test = () => {
+  return (
+    <div className={cx('container')}>
+      <Alram />
+      <HeaderProfile />
+      <Avatar type='review' profileImageUrl='' />
+    </div>
+  );
+};
+
+export default Test;
