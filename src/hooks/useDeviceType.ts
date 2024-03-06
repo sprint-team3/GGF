3691a948a0f9d0cdcc2c6e96ae1cb1aa55abf13a
ccwnc <ccwnc337@gmail.com>
2024-03-07 00:42:52 +0900

    feat: useDeviceType 커스텀 훅 구현

---
+++ src/hooks/useDeviceType.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+
+import { DeviceType } from '@/types';
+
+export const useDeviceType = (): DeviceType => {
+  const [currentDeviceType, setCurrentDeviceType] = useState<DeviceType>(detectDeviceType);
+
+  function detectDeviceType(): DeviceType {
+    const windowWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
+
+    if (windowWidth <= 767) {
+      return DeviceType.Mobile;
+    } else if (windowWidth <= 1199) {
+      return DeviceType.Tablet;
+    } else {
+      return DeviceType.PC;
+    }
+  }
+
+  useEffect(() => {
+    function handleResize() {
+      const newDeviceType = detectDeviceType();
+      setCurrentDeviceType(newDeviceType);
+    }
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return currentDeviceType;
+};
