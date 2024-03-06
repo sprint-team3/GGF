9abca3747f25dadf2c5cbd54ed376cb8014f7f42
grapefruit13 <liam.heo11@gmail.com>
2024-03-07 00:02:36 +0900

    feat: app.tsx에서 queryClientProvider와 devtools 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import type { AppProps } from 'next/app';
+
 import '@/styles/base/common.scss';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { queryClient } from '@/utils';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
