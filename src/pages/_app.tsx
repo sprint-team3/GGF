8c90896962ce4072add24ae92e092761aa0d3dc2
designsoo <kheesoo10@gmail.com>
2024-03-07 03:57:44 +0900

    feat: react-modal 전역 선언

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from 'next/app';
+
+import Modal from 'react-modal';
 import '@/styles/base/common.scss';
+
+Modal.setAppElement('#__next');
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
