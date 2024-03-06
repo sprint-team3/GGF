4bc6fce839d34e9b8228081e1df6724afdc4c491
grapefruit13 <liam.heo11@gmail.com>
2024-03-06 14:47:15 +0900

    chore: scss 사용할 수 있게 next config 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
     additionalData: `@import "@/styles/main.scss";`,
   },
   images: {
