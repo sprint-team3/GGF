5d15f60e9ea1d0ca1f7e138a04a755a4a2d079fb
designsoo <kheesoo10@gmail.com>
2024-03-07 03:57:44 +0900

    feat: react-modal 전역 선언

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import type { AppProps } from 'next/app';
 
+import Modal from 'react-modal';
 import '@/styles/base/common.scss';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import { queryClient } from '@/utils';
+
+Modal.setAppElement('#__next');
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
