c995aa8830046b07a237842d3ac7c501cc17fa09
ccwnc <ccwnc337@gmail.com>
2024-03-07 15:14:33 +0900

    chore: test 파일 수정

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -2,7 +2,7 @@ import classNames from 'classnames/bind';
 
 import { Alram } from '@/components/commons/Alram';
 import { Avatar } from '@/components/commons/Avatar';
-import { HeaderProfile } from '@/components/commons/HeaderProfile';
+import { HeaderProfile } from '@/components/HeaderProfile';
 
 import styles from './Test.module.scss';
 
@@ -13,9 +13,9 @@ const Test = () => {
     <div className={cx('container')}>
       <Alram />
       <HeaderProfile />
-      <Avatar type='header' profileImageUrl='' />
-      <Avatar type='review' profileImageUrl='' />
-      <Avatar type='popup' profileImageUrl='' />
+      <Avatar size='small' profileImageUrl='' />
+      <Avatar size='middle' profileImageUrl='' />
+      <Avatar size='large' profileImageUrl='' />
     </div>
   );
 };
