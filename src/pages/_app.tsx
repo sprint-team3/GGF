9c2845ee387971ca2f627712767e400cb313df1d
grapefruit13 <liam.heo11@gmail.com>
2024-03-07 17:29:58 +0900

    feat: queryClinet 생성 및 app에서 queryClient Provider와 devtools 사용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import type { AppProps } from 'next/app';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Modal from 'react-modal';
+
 import '@/styles/base/common.scss';
+import { queryClient } from '@/utils/queryClient';
 
 Modal.setAppElement('#__next');
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
