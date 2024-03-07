2e82feb5aa849c3b4b9305bda6372777d0a226db
grapefruit13 <liam.heo11@gmail.com>
2024-03-07 14:08:45 +0900

    refactor: app.tsx에서 react modal과의 confilct 해결

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from 'next/app';
 
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Modal from 'react-modal';
+
+import { queryClient } from '@/utils';
 import '@/styles/base/common.scss';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
@@ -8,10 +12,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '@/utils';
 
 Modal.setAppElement('#__next');
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-import { queryClient } from '@/utils';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
