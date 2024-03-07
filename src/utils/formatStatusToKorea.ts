551766aaf87e98b46eb99eab14fd39535cf55bfa
designsoo <kheesoo10@gmail.com>
2024-03-07 16:41:04 +0900

    refactor: 유틸함수에 예약 상태 변수 적용

--- src/utils/formatStatusToKorea.ts
+++ src/utils/formatStatusToKorea.ts
@@ -1,9 +1,5 @@
+import { MY_RESERVATIONS_STATUS } from '@/constants';
+
 import { MyReservationsStatus } from '@/types';
 
-export const formatStatusToKorea = (status: MyReservationsStatus) => {
-  if (status === 'pending') return '신청';
-  if (status === 'confirmed') return '승인';
-  if (status === 'declined') return '거절';
-  if (status === 'canceled') return '취소';
-  if (status === 'completed') return '종료';
-};
+export const formatStatusToKorea = (status: MyReservationsStatus) => MY_RESERVATIONS_STATUS[status];
