90673cb5ce38c02dcb6787076bd3e014bc469b2c
designsoo <kheesoo10@gmail.com>
2024-03-08 21:37:50 +0900

    feat: createdAt 변경하는 getFormatDate 유틸 함수 구현 및 파일명 변경

---
+++ src/utils/getDate.ts
@@ -0,0 +1,16 @@
+import dayjs, { extend, locale } from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
+import 'dayjs/locale/ko';
+
+export const getDiffDate = (date: Date) => {
+  extend(utc);
+  extend(relativeTime);
+  locale('ko');
+
+  return dayjs().to(dayjs(date).utc().format('YYYY-MM-DD HH:mm:ss'));
+};
+
+export const getFormatDate = (date: Date) => {
+  return dayjs(date).format('YYYY년 MM년 DD일');
+};
