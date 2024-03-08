c8f40275b9e2916f26b2cfc308592d5bf21a4f1f
designsoo <kheesoo10@gmail.com>
2024-03-08 16:45:15 +0900

    chore: star 아이콘 이미지 업로드 및 변수 선언

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -4,4 +4,14 @@ export const SVGS = {
     url: '/svgs/profile.svg',
     alt: '프로필',
   },
+  star: {
+    empty: {
+      url: '/svgs/ic-star-empty.svg',
+      alt: '빈 별 아이콘',
+    },
+    filled: {
+      url: '/svgs/ic-star-filled.svg',
+      alt: '채워진 별 아이콘',
+    },
+  },
 };
