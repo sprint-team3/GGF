b880b1ed7661521e12a5c9010374e3212ef5d620
ccwnc <ccwnc337@gmail.com>
2024-03-08 22:38:20 +0900

    refactor: Avatar 컴포넌트 수정

--- src/components/commons/Avatar/index.tsx
+++ src/components/commons/Avatar/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 import classNames from 'classnames/bind';
 
-import { AVATAR_SIZE, SVGS } from '@/constants';
+import { SVGS } from '@/constants';
 
 import styles from './Avatar.module.scss';
 
@@ -15,25 +15,21 @@ type AvatarProps = {
 };
 
 export const Avatar = ({ size, profileImageUrl, isActive }: AvatarProps) => {
+  const imageSize = size === 'large' ? 32 : 20;
+
   return (
-    <div className={cx('container')}>
-      <Image
-        src={isActive ? SVGS.profile.active.url : SVGS.profile.inactive.url}
-        alt={isActive ? SVGS.profile.active.alt : SVGS.profile.inactive.alt}
-        width={AVATAR_SIZE.frame[size]}
-        height={AVATAR_SIZE.frame[size]}
-        priority
-      />
-      {profileImageUrl && (
-        <Image
-          className={cx(size)}
-          src={profileImageUrl}
-          alt='profileImage'
-          width={AVATAR_SIZE.image[size]}
-          height={AVATAR_SIZE.image[size]}
-          priority
-        />
-      )}
+    <div className={cx('outer-frame', `outer-${size}`)}>
+      <div className={cx('dot', 'top', { isActive })}></div>
+      <div className={cx('dot', 'right', { isActive })}></div>
+      <div className={cx('dot', 'bottom', { isActive })}></div>
+      <div className={cx('dot', 'left', { isActive })}></div>
+      <div className={cx('inner-frame', `inner-${size}`, { isActive })}>
+        {profileImageUrl ? (
+          <Image src={profileImageUrl} alt='profileImage' layout='fill' />
+        ) : (
+          <Image src={SVGS.profile.default.url} alt={SVGS.profile.default.alt} width={imageSize} height={imageSize} />
+        )}
+      </div>
     </div>
   );
 };
