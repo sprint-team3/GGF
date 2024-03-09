a7714088015a63b07bd9facd85d3466770b2c289
designsoo <kheesoo10@gmail.com>
2024-03-09 16:48:35 +0900

    feat: 공통 Review Card 컴포넌트 구현

---
+++ src/components/commons/ReviewCard/index.tsx
@@ -0,0 +1,39 @@
+import classNames from 'classnames/bind';
+
+import { getFormatDate } from '@/utils';
+
+import { Avatar } from '@/components/commons/Avatar';
+import StarRating from '@/components/commons/StarRating';
+
+import { Reviewer } from '@/types';
+
+import styles from './ReviewCard.module.scss';
+
+const cx = classNames.bind(styles);
+
+type ReviewCardProps = {
+  user: Reviewer;
+  rating: number;
+  date: Date;
+  content: string;
+};
+
+const ReviewCard = ({ user, rating, date, content }: ReviewCardProps) => {
+  return (
+    <article className={cx('review-card')}>
+      <div className={cx('review-card-profile')}>
+        <Avatar size='small' profileImageUrl={user.profileImageUrl} />
+        <div className={cx('review-card-info')}>
+          <h1 className={cx('review-card-info-reviewer')}>{user.nickname}</h1>
+          <div className={cx('review-card-info-rating')}>
+            <StarRating size='small' rating={rating} />
+            <span className={cx('review-card-info-date')}>{getFormatDate(date)}</span>
+          </div>
+        </div>
+      </div>
+      <p className={cx('review-card-comment')} dangerouslySetInnerHTML={{ __html: content }}></p>
+    </article>
+  );
+};
+
+export default ReviewCard;
