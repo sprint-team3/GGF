be026936d471f75fb4c927e751ccf09cf75cc626
CheeseB <2489ckckck@naver.com>
2024-03-10 05:58:47 +0900

    fix: image 상수 충돌 해결

---
+++ src/components/commons/buttons/OperationButton.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+import { MouseEventHandler } from 'react';
+
+import classNames from 'classnames/bind';
+
+import { SVGS } from '@/constants';
+
+import styles from './OperationButton.module.scss';
+
+const cx = classNames.bind(styles);
+
+type OperationButtonProps = {
+  type: 'add' | 'remove';
+  disabled?: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const OperationButton = ({ type, disabled, onClick }: OperationButtonProps) => {
+  const buttonType = type === 'add' && disabled ? 'disabled' : type;
+
+  return (
+    <button className={cx(`btn-${type}`)} disabled={disabled} onClick={onClick}>
+      <div className={cx('btn-icon')}>
+        <Image src={SVGS.button[buttonType].url} alt={SVGS.button[buttonType].alt} fill />
+      </div>
+    </button>
+  );
+};
