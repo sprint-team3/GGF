ca5590acaba2d975abb898e4fc9d250325bca5d3
ccwnc <ccwnc337@gmail.com>
2024-03-11 02:27:57 +0900

    refactor: 피드백 반영

--- src/components/layout/header/Menu/index.tsx
+++ src/components/layout/header/Menu/index.tsx
@@ -1,31 +1,42 @@
+import Link from 'next/link';
+
 import { useState } from 'react';
 
 import classNames from 'classnames/bind';
+
+import { GAME_LIST } from '@/constants';
 
 import styles from './Menu.module.scss';
 
 const cx = classNames.bind(styles);
 
 export const Menu = () => {
-  const gameTypes = ['LEAGUE OF LEGENDS', 'BATTLEGROUNDS', 'OVERWATCH 2', 'MINCRAFT'];
   const [activatedGameType, setActivatedGameType] = useState<number>(0);
 
   const handleActivateGameType = (number: number) => {
     setActivatedGameType(number);
   };
 
+  const formatGameTypeToLink = (type: string) => {
+    return type.toLowerCase().replace(/\s+/g, '-');
+  };
+
   return (
-    <div className={cx('container')}>
-      {gameTypes.map((type, index) => (
-        <button
-          className={cx('button', { activated: activatedGameType === index })}
-          key={index}
-          onClick={() => handleActivateGameType(index)}
-        >
-          <p>{type}</p>
-          {activatedGameType === index && <p className={cx('under-line')}></p>}
-        </button>
-      ))}
-    </div>
+    <nav>
+      <ul className={cx('container')}>
+        {GAME_LIST.map((type, index) => (
+          <li key={`menu-${index}`}>
+            <Link
+              href={formatGameTypeToLink(type)}
+              className={cx('game', { activated: activatedGameType === index })}
+              onClick={() => handleActivateGameType(index)}
+            >
+              {type}
+              {activatedGameType === index && <p className={cx('under-line')}></p>}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
