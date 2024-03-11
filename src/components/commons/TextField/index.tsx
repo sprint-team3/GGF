35d19970deb04c0834e5e44d2bc2d76086566dd3
grapefruit13 <liam.heo11@gmail.com>
2024-03-11 15:24:22 +0900

    refactor: a11y 해제, keyDown 이벤트 삭제, state이름 변경

--- src/components/commons/TextField/index.tsx
+++ src/components/commons/TextField/index.tsx
@@ -1,4 +1,5 @@
-import { ChangeEvent, KeyboardEvent, useState } from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { ChangeEvent, useState } from 'react';
 
 import classNames from 'classnames/bind';
 import { useFormContext } from 'react-hook-form';
@@ -20,8 +21,8 @@ const TextField = ({ label, name, maxLength = 700, ...props }: TextFieldProps) =
     register,
     formState: { errors },
   } = useFormContext();
-  const [textcount, setTextcount] = useState<number>(0);
-  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [textCount, setTextCount] = useState(0);
+  const [isFocused, setIsFocused] = useState(false);
 
   const isError = !!errors[name]?.message;
 
@@ -33,16 +34,8 @@ const TextField = ({ label, name, maxLength = 700, ...props }: TextFieldProps) =
     setIsFocused(false);
   };
 
-  const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-    if (event.key == 'Enter') {
-      if (!setIsFocused) {
-        handleClick();
-      }
-    }
-  };
-
   const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
-    setTextcount(event.target.textLength);
+    setTextCount(event.target.textLength);
   };
 
   return (
@@ -54,7 +47,6 @@ const TextField = ({ label, name, maxLength = 700, ...props }: TextFieldProps) =
         tabIndex={0}
         onClick={handleClick}
         onBlur={handleBlur}
-        onKeyDown={handleKeyDown}
       >
         <textarea
           className={cx('text-field-text-group-textarea')}
@@ -64,7 +56,7 @@ const TextField = ({ label, name, maxLength = 700, ...props }: TextFieldProps) =
           {...props}
         />
         <div className={cx('text-field-text-group-footer')}>
-          <span className={cx('text-field-text-group-footer-current-num', { active: textcount > 0 })}>{textcount}</span>
+          <span className={cx('text-field-text-group-footer-current-num', { active: textCount > 0 })}>{textCount}</span>
           <span className={cx('text-field-text-group-footer-total-num')}>/{maxLength}</span>
         </div>
       </div>
