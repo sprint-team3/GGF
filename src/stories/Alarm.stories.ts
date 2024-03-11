49c0de47d3eb22bf41676db333c9e8e0e8866ae4
ccwnc <ccwnc337@gmail.com>
2024-03-11 22:46:47 +0900

    fix: Alarm 컴포넌트 import 경로 및 args 수정

--- src/stories/Alarm.stories.ts
+++ src/stories/Alarm.stories.ts
@@ -1,4 +1,4 @@
-import { Alarm } from '@/components/commons/Alarm';
+import { Alarm } from '@/components/layout/header/Alarm';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
@@ -12,7 +12,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Example: Story = {
   args: {
-    isExistedAlarm: false,
+    isAlarmExisted: false,
     isActivated: false,
   },
 };
