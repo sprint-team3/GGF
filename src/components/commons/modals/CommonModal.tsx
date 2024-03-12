47d2b014584207460fb4a250cc37adb967376e05
designsoo <kheesoo10@gmail.com>
2024-03-13 02:23:41 +0900

    refactor: 모달 꾸밈요소 가상 요소로 변경

---
+++ src/components/commons/modals/CommonModal.tsx
@@ -0,0 +1,38 @@
+import { ReactNode, MouseEventHandler } from 'react';
+
+import classNames from 'classnames/bind';
+import ReactModal from 'react-modal';
+
+import styles from './CommonModal.module.scss';
+
+const cx = classNames.bind(styles);
+
+type CommonModalProps = {
+  openModal: boolean;
+  onClose: MouseEventHandler<HTMLButtonElement>;
+  title: string;
+  renderContent: ReactNode;
+};
+
+export const CommonModal = ({ openModal, onClose, title, renderContent }: CommonModalProps) => {
+  return (
+    <ReactModal
+      isOpen={openModal}
+      onRequestClose={onClose}
+      closeTimeoutMS={250}
+      shouldCloseOnEsc={true}
+      shouldCloseOnOverlayClick={true}
+      className={cx('modal')}
+      overlayClassName={cx('overlay')}
+      bodyOpenClassName={cx('body-open')}
+      contentLabel='modal-common'
+    >
+      <div className={cx('modal-inner')}>
+        <header className={cx('modal-header')}>
+          <h1 className={cx('modal-header-title')}>{title}</h1>
+        </header>
+        <main className={cx('modal-content')}>{renderContent}</main>
+      </div>
+    </ReactModal>
+  );
+};
