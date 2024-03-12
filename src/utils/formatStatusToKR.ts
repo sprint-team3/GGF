d538e2709a77562c214d7dd9178f8430c6fb5997
designsoo <kheesoo10@gmail.com>
2024-03-13 03:14:13 +0900

    feat: 모집타입 문자열 포맷팅 유틸함수 구현

--- src/utils/formatStatusToKR.ts
+++ src/utils/formatStatusToKR.ts
@@ -1,5 +1,9 @@
 import { MY_RESERVATIONS_STATUS } from '@/constants';
 
-import { MyReservationsStatus } from '@/types';
+import { RECRUIT_TYPES } from '@/constants/recruitType';
+
+import { MyReservationsStatus, RecruitTypes } from '@/types';
 
 export const formatStatusToKR = (status: MyReservationsStatus) => MY_RESERVATIONS_STATUS[status];
+
+export const formatRecruitTypeToKR = (recruitType: RecruitTypes) => RECRUIT_TYPES[recruitType];
