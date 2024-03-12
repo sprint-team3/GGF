8c9b7bebbe3f28408aa4a14cd692a1adbf14baee
designsoo <kheesoo10@gmail.com>
2024-03-13 03:18:20 +0900

    refactor: 클랜모집, 게임공략 태그 추가 및 스타일링 적용

--- src/components/commons/Tag/index.tsx
+++ src/components/commons/Tag/index.tsx
@@ -1,17 +1,19 @@
 import classNames from 'classnames/bind';
 
+import { formatRecruitTypeToKR } from '@/utils';
+
+import { RecruitTypes } from '@/types';
+
 import styles from './Tag.module.scss';
 
 const cx = classNames.bind(styles);
 
 type TagProps = {
-  price: number;
+  recruitType: RecruitTypes;
 };
 
-const Tag = ({ price }: TagProps) => {
-  const recruitmentType = price === 0 ? 'offline' : 'online';
-
-  return <div className={cx(`tag-${recruitmentType}`)}>{recruitmentType}</div>;
+const Tag = ({ recruitType }: TagProps) => {
+  return <div className={cx(`tag-${recruitType}`)}>{formatRecruitTypeToKR(recruitType)}</div>;
 };
 
 export default Tag;
