f7a942dc3dbec1fb5e4cf791ec835c42740ad42d
grapefruit13 <liam.heo11@gmail.com>
2024-03-12 13:16:38 +0900

    refactor: useEffect, useRef삭제하고  defaultChecked 옵션 사용

--- src/components/commons/InputRadio/index.tsx
+++ src/components/commons/InputRadio/index.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-
 import classNames from 'classnames/bind';
 import { useFormContext } from 'react-hook-form';
 
@@ -17,21 +15,20 @@ type InputRadioProps = {
 const InputRadio = ({ legend, name }: InputRadioProps) => {
   const { register } = useFormContext();
 
-  const firstInputRef = useRef<HTMLLabelElement>(null);
-
-  useEffect(() => {
-    if (firstInputRef.current) {
-      firstInputRef.current.click();
-    }
-  }, []);
-
   return (
     <fieldset className={cx('input-radio')}>
       <legend className={cx('input-radio-legend')}>{legend}</legend>
       {RecruitTypes.map((option, index) => (
         <div className={cx('input-radio-option-container')} key={option.id}>
-          <input className={cx('radio')} type='radio' id={option.id} value={option.value} {...register(name)} />
-          <label className={cx('label')} htmlFor={option.id} ref={index === 0 ? firstInputRef : null}>
+          <input
+            className={cx('radio')}
+            type='radio'
+            id={option.id}
+            value={option.value}
+            defaultChecked={index === 0}
+            {...register(name)}
+          />
+          <label className={cx('label')} htmlFor={option.id}>
             {option.label}
           </label>
         </div>
