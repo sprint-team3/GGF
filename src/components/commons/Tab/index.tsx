ec5202993912c7c05ef01e755f452e1cf137b895
CheeseB <2489ckckck@naver.com>
2024-03-13 23:32:41 +0900

    feat: 공용 탭 컴포넌트 구현

---
+++ src/components/commons/Tab/index.tsx
@@ -0,0 +1,44 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import classNames from 'classnames/bind';
+
+import styles from './Tab.module.scss';
+
+const cx = classNames.bind(styles);
+
+type TabItem = {
+  id: string;
+  text: string;
+  count?: number;
+};
+
+type TabProps = {
+  items: TabItem[];
+  size: 'small' | 'medium';
+  selectedTabId: string;
+  setSelectedTabId: Dispatch<SetStateAction<string>>;
+};
+
+export const Tab = ({ items, size, selectedTabId, setSelectedTabId }: TabProps) => {
+  const isActivated = (id: string) => id === selectedTabId;
+
+  const handleClickTabItem = (clickedItemId: string) => {
+    if (!isActivated(clickedItemId)) setSelectedTabId(clickedItemId);
+  };
+
+  return (
+    <ul className={cx('tab')}>
+      {items.map((item, index) => (
+        <li key={`item-${index}`}>
+          <button
+            className={cx(`tab-item-${size}`, { activated: isActivated(item.id) })}
+            onClick={() => handleClickTabItem(item.id)}
+          >
+            <span>{item.text}</span>
+            {item.count !== undefined && <span className={cx('tab-item-count')}>{item.count}</span>}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
