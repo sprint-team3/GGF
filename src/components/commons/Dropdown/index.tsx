5d7db08d1b5eb5657887ee782c90af2ca0f0109a
grapefruit13 <liam.heo11@gmail.com>
2024-03-13 20:41:15 +0900

    refactor: 화살표 애니메이션 추가, list 위치 변경 등

--- src/components/commons/Dropdown/index.tsx
+++ src/components/commons/Dropdown/index.tsx
@@ -15,6 +15,9 @@ import styles from './Dropdown.module.scss';
 
 const cx = classNames.bind(styles);
 
+const { url: defaultUrl, alt: defaultAlt } = SVGS.arrow.down.default;
+const { url: activeUrl, alt: activeAlt } = SVGS.arrow.down.active;
+
 type DropdownProps = {
   name: string;
   options: (number | string)[] | { id: number; title: string }[];
@@ -70,11 +73,17 @@ const Dropdown = ({
             onClick={togglePopup}
             readOnly
           />
-          <button className={cx('arrow-btn')} ref={buttonRef} type='button' disabled={isDisabled} onClick={togglePopup}>
+          <button
+            className={cx('arrow-btn', { rotate: isOpen })}
+            ref={buttonRef}
+            type='button'
+            disabled={isDisabled}
+            onClick={togglePopup}
+          >
             {isOpen ? (
-              <Image src={SVGS.arrow.up.url} alt={SVGS.arrow.up.alt} width={24} height={24} />
+              <Image src={activeUrl} alt={activeAlt} width={24} height={24} />
             ) : (
-              <Image src={SVGS.arrow.down.url} alt={SVGS.arrow.down.alt} width={24} height={24} />
+              <Image src={defaultUrl} alt={defaultAlt} width={24} height={24} />
             )}
           </button>
         </div>
