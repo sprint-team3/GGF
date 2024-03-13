aa5208a3c00408f8d13ff473154d909f553ec0fc
designsoo <kheesoo10@gmail.com>
2024-03-13 18:11:05 +0900

    refactor: more 버튼 컴포넌트 적용, address 옵셔널 파라미터 변경 및 이미지 sizes 속성 제거

--- src/components/commons/cards/RegisteredCard.tsx
+++ src/components/commons/cards/RegisteredCard.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { MouseEventHandler } from 'react';
+
 import classNames from 'classnames/bind';
 
 import { SVGS } from '@/constants';
 import { getFormatDate } from '@/utils';
 
+import { MoreButton } from '@/components/commons/buttons';
 import Tag from '@/components/commons/Tag';
 
 import { GameCategory, PostTypes } from '@/types';
@@ -19,12 +22,23 @@ export type RegisteredCardProps = {
   path: string;
   postType: PostTypes;
   title: string;
-  address: string;
+  address?: string;
   category: GameCategory;
   createdAt: string;
+  isOpen: boolean;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-const RegisteredCard = ({ path, postType, title, address, category, createdAt }: RegisteredCardProps) => {
+const RegisteredCard = ({
+  path,
+  postType,
+  title,
+  address,
+  category,
+  createdAt,
+  isOpen,
+  onClick,
+}: RegisteredCardProps) => {
   const isOffline = postType === 'offline';
 
   return (
@@ -36,19 +50,21 @@ const RegisteredCard = ({ path, postType, title, address, category, createdAt }:
               <Tag postType={postType} />
               <span className={cx('card-content-header-category-game')}>{category}</span>
             </div>
-            <button className={cx('card-content-header-kebab')}>kebab</button>
+            <div className={cx('card-content-header-kebab')}>
+              <MoreButton isActive={isOpen} onClick={onClick} />
+            </div>
           </header>
           <h1 className={cx('card-content-title')}>{title}</h1>
           {isOffline && (
             <div className={cx('card-content-location')}>
-              <Image src={location.default.url} alt={location.default.alt} width={18} height={18} sizes='100%' />
+              <Image src={location.default.url} alt={location.default.alt} width={18} height={18} />
               <span className={cx('card-content-location-address')}>{address}</span>
             </div>
           )}
         </main>
         <footer className={cx('card-footer')}>
           <div className={cx('card-footer-calendar')}>
-            <Image src={calendar.default.url} alt={calendar.default.alt} width={20} height={20} sizes='100%' />
+            <Image src={calendar.default.url} alt={calendar.default.alt} width={20} height={20} />
             <span className={cx('card-footer-calendar-date')}>{getFormatDate(createdAt)}</span>
           </div>
         </footer>
