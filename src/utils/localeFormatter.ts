d1c6a6f949fc093e593f7c81826fe75be7064d47
designsoo <kheesoo10@gmail.com>
2024-03-13 12:54:21 +0900

    refactor: 변경된 포스트타입 일괄 적용

--- src/utils/localeFormatter.ts
+++ src/utils/localeFormatter.ts
@@ -1,7 +1,7 @@
-import { MY_RESERVATIONS_STATUS, RECRUIT_TYPES } from '@/constants';
+import { MY_RESERVATIONS_STATUS, POST_TYPES } from '@/constants';
 
-import { MyReservationsStatus, RecruitTypes } from '@/types';
+import { MyReservationsStatus, PostTypes } from '@/types';
 
 export const formatStatusToKR = (status: MyReservationsStatus) => MY_RESERVATIONS_STATUS[status];
 
-export const formatRecruitTypeToKR = (recruitType: RecruitTypes) => RECRUIT_TYPES[recruitType];
+export const formatRecruitTypeToKR = (postType: PostTypes) => POST_TYPES[postType];
