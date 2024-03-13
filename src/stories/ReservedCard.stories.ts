3c9934c011c206af3b4c44cad8d083234c1946d8
designsoo <kheesoo10@gmail.com>
2024-03-13 16:23:20 +0900

    feat: 예약된 카드 컴포넌트 스토리북 적용

---
+++ src/stories/ReservedCard.stories.ts
@@ -0,0 +1,25 @@
+import ReservedCard from '@/components/commons/cards/ReservedCard';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta = {
+  title: 'Cards/ReservedCard',
+  component: ReservedCard,
+} satisfies Meta<typeof ReservedCard>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Example: Story = {
+  args: {
+    path: '/',
+    status: 'pending',
+    postType: 'offline',
+    title: '[PC방 대회]리그 오브 레전드 개발자들의 PC방 습격!',
+    address: '강남 SBXG 포탈 PC방',
+    category: '리그오브레전드',
+    createdAt: '2024-03-13T04:44:02.924Z',
+    date: '2024-03-13',
+    endTime: '18:00',
+  },
+};
