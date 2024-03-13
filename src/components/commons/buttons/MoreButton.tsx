ac9d456e40aaab102055b89575a719dba434fe1b
designsoo <kheesoo10@gmail.com>
2024-03-13 18:12:14 +0900

    refactor: isActive props 추가 및 active SCSS 속성 추가

--- src/components/commons/buttons/MoreButton.tsx
+++ src/components/commons/buttons/MoreButton.tsx
@@ -6,20 +6,21 @@ import classNames from 'classnames/bind';
 
 import { SVGS } from '@/constants';
 
-import styles from './ArrowButton.module.scss';
+import styles from './MoreButton.module.scss';
 
 const cx = classNames.bind(styles);
 
 type MoreButtonProps = {
   onClick: MouseEventHandler<HTMLButtonElement>;
+  isActive: boolean;
 };
 
-export const MoreButton = ({ onClick }: MoreButtonProps) => {
+export const MoreButton = ({ onClick, isActive }: MoreButtonProps) => {
   const { url, alt } = SVGS.button.more;
 
   return (
-    <button className={cx('btn')} onClick={onClick}>
-      <div className={cx('btn-icon')}>
+    <button className={cx('btn-more')} aria-label='메뉴 더보기 버튼' aria-pressed={isActive} onClick={onClick}>
+      <div className={cx('btn-more-icon')}>
         <Image src={url} alt={alt} fill />
       </div>
     </button>
