3cdd0110e8fcb1dbc24275c865592bc52a22f9a5
ccwnc <ccwnc337@gmail.com>
2024-03-13 19:39:31 +0900

    refactor: 피드백 반영

--- src/components/layout/header/UserMenu/index.tsx
+++ src/components/layout/header/UserMenu/index.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { RefObject } from 'react';
+
 import classNames from 'classnames/bind';
 
-import { SVGS } from '@/constants';
+import { PAGE_PATHS, SVGS } from '@/constants';
 import { signout } from '@/utils';
 
 import { Avatar } from '@/components/commons/Avatar';
@@ -12,12 +14,13 @@ import styles from './UserMenu.module.scss';
 
 const cx = classNames.bind(styles);
 const { url, alt } = SVGS.button.setting;
+const { account, mypage } = PAGE_PATHS;
 
 type UserMenuProps = {
   profileImageUrl: string;
   nickname: string;
   email: string;
-  userMenuRef: React.RefObject<HTMLDivElement>;
+  userMenuRef: RefObject<HTMLDivElement>;
 };
 
 export const UserMenu = ({ profileImageUrl, nickname, email, userMenuRef }: UserMenuProps) => {
@@ -28,7 +31,7 @@ export const UserMenu = ({ profileImageUrl, nickname, email, userMenuRef }: User
         <div className={cx('container-user-info')}>
           <div className={cx('container-nickname')}>
             <p className={cx('nickname')}>{nickname}</p>
-            <Link href={'/user/account'}>
+            <Link href={account}>
               <Image src={url} alt={alt} width={16} height={16} />
             </Link>
           </div>
@@ -36,10 +39,10 @@ export const UserMenu = ({ profileImageUrl, nickname, email, userMenuRef }: User
         </div>
       </div>
       <div className={cx('container-button')}>
-        <Link className={cx('button', 'mypage')} href={'/mypage'}>
+        <Link className={cx('container-button-mypage')} href={mypage}>
           My Page
         </Link>
-        <button className={cx('button', 'logout')} onClick={signout}>
+        <button className={cx('container-button-logout')} onClick={signout}>
           Logout
         </button>
       </div>
