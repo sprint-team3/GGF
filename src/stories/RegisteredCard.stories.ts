018c2477b3e53742d4409c71941ce036296a6719
designsoo <kheesoo10@gmail.com>
2024-03-15 01:23:30 +0900

    refactor: 등록한 모집 카드 스토리북 isOpen props 삭제

--- src/stories/RegisteredCard.stories.ts
+++ src/stories/RegisteredCard.stories.ts
@@ -17,7 +17,6 @@ export const Example: Story = {
     title: '[PC방 대회]리그 오브 레전드 개발자들의 PC방 습격!',
     address: '강남 SBXG 포탈 PC방',
     category: '리그오브레전드',
-    isOpen: false,
     createdAt: '2024-03-13T04:44:02.924Z',
   },
 };
