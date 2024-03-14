795a7645e59c30c1068e58e4844e3bd6ed111ed8
designsoo <kheesoo10@gmail.com>
2024-03-15 01:21:52 +0900

    refactor: 케밥 컴포넌트 추가 및 props 수정

--- src/components/commons/cards/RegisteredCard.tsx
+++ src/components/commons/cards/RegisteredCard.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import { MouseEventHandler } from 'react';
-
 import classNames from 'classnames/bind';
 
 import { SVGS } from '@/constants';
 import { getFormatDate } from '@/utils';
 
-import { MoreButton } from '@/components/commons/buttons';
+import { ConfirmModal } from '../modals';
+
+import Kebabmenu from '@/components/commons/Kebabmenu';
+import { ModalButton } from '@/components/commons/modals/ModalButton';
 import Tag from '@/components/commons/Tag';
+import useMultiState from '@/hooks/useMultiState';
 
 import { GameCategory, PostTypes } from '@/types';
 
@@ -25,51 +27,69 @@ export type RegisteredCardProps = {
   address?: string;
   category: GameCategory;
   createdAt: string;
-  isOpen: boolean;
-  onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-const RegisteredCard = ({
-  path,
-  postType,
-  title,
-  address,
-  category,
-  createdAt,
-  isOpen,
-  onClick,
-}: RegisteredCardProps) => {
+const RegisteredCard = ({ path, postType, title, address, category, createdAt }: RegisteredCardProps) => {
+  const { multiState, toggleClick } = useMultiState(['cancelReservationModal']);
   const isOffline = postType === 'offline';
 
+  const handleCommonModal = (modalKey: string) => {
+    toggleClick(modalKey);
+  };
+
+  const handleSelectedClick = (value: string) => {
+    if (value === '삭제') {
+      handleCommonModal('cancelReservationModal');
+    } else {
+      window.location.href = '/minecraft';
+    }
+  };
+
   return (
-    <article className={cx('card')}>
-      <Link href={path} className={cx('card-inner')}>
-        <main className={cx('card-content')}>
-          <header className={cx('card-content-header')}>
-            <div className={cx('card-content-header-category')}>
-              <Tag postType={postType} />
-              <span className={cx('card-content-header-category-game')}>{category}</span>
+    <>
+      <article className={cx('card')}>
+        <Link href={path} className={cx('card-inner')}>
+          <main className={cx('card-content')}>
+            <header className={cx('card-content-header')}>
+              <div className={cx('card-content-header-category')}>
+                <Tag postType={postType} />
+                <span className={cx('card-content-header-category-game')}>{category}</span>
+              </div>
+              <div className={cx('card-content-header-kebab')}>
+                <Kebabmenu onClick={handleSelectedClick} />
+              </div>
+            </header>
+            <h1 className={cx('card-content-title')}>{title}</h1>
+            {isOffline && (
+              <div className={cx('card-content-location')}>
+                <Image src={location.default.url} alt={location.default.alt} width={18} height={18} />
+                <span className={cx('card-content-location-address')}>{address}</span>
+              </div>
+            )}
+          </main>
+          <footer className={cx('card-footer')}>
+            <div className={cx('card-footer-calendar')}>
+              <Image src={calendar.default.url} alt={calendar.default.alt} width={20} height={20} />
+              <span className={cx('card-footer-calendar-date')}>{getFormatDate(createdAt)}</span>
             </div>
-            <div className={cx('card-content-header-kebab')}>
-              <MoreButton isActive={isOpen} onClick={onClick} />
-            </div>
-          </header>
-          <h1 className={cx('card-content-title')}>{title}</h1>
-          {isOffline && (
-            <div className={cx('card-content-location')}>
-              <Image src={location.default.url} alt={location.default.alt} width={18} height={18} />
-              <span className={cx('card-content-location-address')}>{address}</span>
-            </div>
-          )}
-        </main>
-        <footer className={cx('card-footer')}>
-          <div className={cx('card-footer-calendar')}>
-            <Image src={calendar.default.url} alt={calendar.default.alt} width={20} height={20} />
-            <span className={cx('card-footer-calendar-date')}>{getFormatDate(createdAt)}</span>
-          </div>
-        </footer>
-      </Link>
-    </article>
+          </footer>
+        </Link>
+      </article>
+
+      <ConfirmModal
+        warning
+        openModal={multiState.cancelReservationModal}
+        onClose={() => handleCommonModal('cancelReservationModal')}
+        state='STOP'
+        title='예약을 취소하시겠습니까?'
+        desc='한 번 취소한 예약은 되돌릴 수 없습니다'
+      >
+        <ModalButton variant='warning' onClick={() => handleCommonModal('cancelReservationModal')}>
+          예약 취소
+        </ModalButton>
+        <ModalButton onClick={() => handleCommonModal('cancelReservationModal')}>닫기</ModalButton>
+      </ConfirmModal>
+    </>
   );
 };
 
