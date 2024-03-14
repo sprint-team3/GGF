238db80f172b6e872d6bb4dd534449ca0a36df37
designsoo <kheesoo10@gmail.com>
2024-03-14 23:15:52 +0900

    feat: FAQ 목록 상수화

---
+++ src/constants/faqList.ts
@@ -0,0 +1,6 @@
+export const FAQ_LIST = [
+  { id: 0, title: 'GGF란?', description: '[GGF란?] 1번 description입니다.' },
+  { id: 1, title: '파티원은 어디서 모집하나요?', description: '[파티 모집] 2번 description입니다.' },
+  { id: 2, title: '클랜은 어디서 모집하나요?', description: '[클랜 모집]3번 description입니다.' },
+  { id: 3, title: 'GGF 크리에이터란?', description: '[크리에이터] 4번 description입니다.' },
+];
