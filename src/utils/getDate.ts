f9a431ac1aeaa37b4058abb510b33fe66159972d
designsoo <kheesoo10@gmail.com>
2024-03-14 23:11:39 +0900

    feat: 현재 시간, 밀리초 변환 유틸함수 구현

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,9 +1,13 @@
 import dayjs, { extend, locale } from 'dayjs';
-import 'dayjs/locale/ko';
 import relativeTime from 'dayjs/plugin/relativeTime';
-import timezone from 'dayjs/plugin/timezone';
-import utcPlugin from 'dayjs/plugin/utc';
-
+import utc from 'dayjs/plugin/utc';
+import 'dayjs/locale/ko';
+/**
+ *
+ * @param date
+ * @param endTime
+ * @returns boolean
+ */
 export const getExpirationDate = (date: string | Date, endTime: string) => {
   const expirationTime = dayjs(`${date} ${endTime}`);
   return expirationTime.diff(dayjs(), 'second') < 0;
@@ -39,4 +43,35 @@ export const getElapsedTimeToKST = (createdAt: string | Date): string => {
   const kstTime = dayjs(createdAt).utc().tz('Asia/Seoul');
 
   return kstTime.fromNow();
+};
+
+/**
+ *
+ * @param date
+ * @returns '00분 전'
+ */
+export const getDiffDate = (date: string | Date) => {
+  extend(utc);
+  extend(relativeTime);
+  locale('ko');
+
+  return dayjs().to(dayjs(date).utc().format('YYYY-MM-DD HH:mm:ss'));
+};
+
+/**
+ *
+ * @returns 'HH:mm'
+ */
+export const getCurrentTime = () => {
+  const currentTime = dayjs().format('HH:mm');
+  return currentTime;
+};
+
+/**
+ * @param timestamp
+ * @returns 'HH:mm'
+ */
+export const formatTimestamp = (timestamp: number) => {
+  const formattedTime = dayjs(timestamp).format('HH:mm');
+  return formattedTime;
 };
