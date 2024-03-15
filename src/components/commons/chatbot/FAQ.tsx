dd1212447920087bb2e732063d92bdf624da136d
designsoo <kheesoo10@gmail.com>
2024-03-15 11:36:12 +0900

    feat: FAQ 컴포넌트 구현 및 faq chat 분리

--- src/components/commons/chatbot/FAQ.tsx
+++ src/components/commons/chatbot/FAQ.tsx
@@ -1,7 +1,5 @@
 import Image from 'next/image';
 
-import { useState } from 'react';
-
 import classNames from 'classnames/bind';
 
 import { SVGS, FAQ_LIST } from '@/constants';
@@ -11,46 +9,26 @@ import styles from './FAQ.module.scss';
 const cx = classNames.bind(styles);
 const { url, alt } = SVGS.chat;
 
-type FaqAnswer = {
-  title: string;
-  description: string;
+type FAQProps = {
+  onClick: (action: number) => void;
 };
 
-export const FAQ = () => {
-  const [faqAnswer, setfaqAnswer] = useState<FaqAnswer[]>([]);
-
-  const handleAddFaq = (id: number) => {
-    setfaqAnswer((prev) => [...prev, FAQ_LIST[id]]);
-  };
-
+export const FAQ = ({ onClick }: FAQProps) => {
   return (
     <div className={cx('faq')}>
-      <div className={cx('faq-menu')}>
-        <p className={cx('faq-menu-title')}>자주 물어보는 질문이에요</p>
+      <article className={cx('faq-menu')}>
+        <h1 className={cx('faq-menu-title')}>자주 물어보는 질문이에요</h1>
         <ul className={cx('faq-question')}>
           {FAQ_LIST.map(({ id, title }) => (
-            <li className={cx('faq-question-list')} key={id}>
-              <button className={cx('faq-question-list-item')} onClick={() => handleAddFaq(id)}>
+            <li className={cx('faq-question-list')} key={`faq-${id}`}>
+              <button className={cx('faq-question-list-item')} onClick={() => onClick(id)}>
                 <Image src={url} alt={alt} width={16} height={16}></Image>
-                <span className={cx('faq-question-list-item-text')}>{title}</span>
+                <span className={cx('faq-question-list-item-title')}>{title}</span>
               </button>
             </li>
           ))}
         </ul>
-      </div>
-
-      <ul className={cx('faq-chat')}>
-        {faqAnswer.map((answer, idx) => (
-          <li className={cx('faq-chat-list')} key={idx}>
-            <div className={cx('faq-chat-list-question')}>
-              <p className={cx('faq-chat-list-question-description')}>{answer.title}</p>
-            </div>
-            <div className={cx('faq-chat-list-answer')}>
-              <p className={cx('faq-chat-list-answer-description')}>{answer.description}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      </article>
     </div>
   );
 };
