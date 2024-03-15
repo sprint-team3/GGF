2966a480be37c5e9f4bf4d0090cc4f6c3100340e
designsoo <kheesoo10@gmail.com>
2024-03-15 12:34:24 +0900

    feat: 챗봇 대화 컴포넌트 구현

--- src/components/commons/chatbot/index.ts
+++ src/components/commons/chatbot/index.ts
@@ -1,3 +1,7 @@
 export * from './Chatbot';
 export * from './ChatbotButton';
+export * from './ChatbotDialog';
+export * from './ChatHeader';
 export * from './FAQ';
+export * from './FAQChatDialog';
+export * from './Loading';
