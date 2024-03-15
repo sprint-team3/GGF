070915e23bfff24f6991d7d91a1a0f6e41a6bd97
designsoo <kheesoo10@gmail.com>
2024-03-15 12:33:45 +0900

    refactor: FAQ 채팅 컴포넌트 구현

--- src/components/commons/chatbot/FAQChatDialog.tsx
+++ src/components/commons/chatbot/FAQChatDialog.tsx
@@ -1,8 +1,6 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import { Fragment, useState } from 'react';
 
 import classNames from 'classnames/bind';
-
-import { FAQ_LIST } from '@/constants';
 
 import { FAQ } from '@/components/commons/chatbot/FAQ';
 
@@ -10,50 +8,38 @@ import styles from './FAQChatDialog.module.scss';
 
 const cx = classNames.bind(styles);
 
-type FaqChat = {
-  id: number;
+type FAQChatDialogProps = {
   title: string;
   description: string;
+  onClick: (action: number) => void;
 };
-export const FAQChatDialog = () => {
-  const messageEndRef = useRef<HTMLDivElement | null>(null);
-  const [faqChat, setFaqChat] = useState<FaqChat[]>([]);
 
-  const handleAddFaq = (id: number) => {
-    setFaqChat((prev) => [...prev, FAQ_LIST[id]]);
+export const FAQChatDialog = ({ title, description, onClick }: FAQChatDialogProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleFaqOpen = () => {
+    setIsOpen(true);
   };
 
-  useEffect(() => {
-    if (messageEndRef.current) {
-      messageEndRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, [faqChat]);
-
   return (
-    <div>
-      <FAQ onClick={handleAddFaq} />
-
-      <ul className={cx('faq-chat')}>
-        {faqChat.map((answer, index) => (
-          <li className={cx('faq-chat-list')} key={index}>
-            <div className={cx('faq-chat-list-question')}>
-              <p className={cx('faq-chat-list-question-description')}>{answer.title}</p>
-            </div>
-            <div className={cx('faq-chat-list-answer')}>
-              <p className={cx('faq-chat-list-answer-description')}>
-                {answer.description.split('\n').map((answer, index) => (
-                  <Fragment key={`answer-${index}`}>
-                    {answer}
-                    <br />
-                  </Fragment>
-                ))}
-              </p>
-            </div>
-          </li>
-        ))}
-      </ul>
-
-      <div ref={messageEndRef}></div>
-    </div>
+    <ul className={cx('faq-chat-list')}>
+      <li className={cx('faq-chat-list-question')}>
+        <p className={cx('faq-chat-list-question-description')}>{title}</p>
+      </li>
+      <li className={cx('faq-chat-list-answer')}>
+        <p className={cx('faq-chat-list-answer-description')}>
+          {description.split('\n').map((answer, index) => (
+            <Fragment key={`answer-${index}`}>
+              {answer}
+              <br />
+            </Fragment>
+          ))}
+        </p>
+      </li>
+      <button className={cx('faq-visibility-button')} onClick={handleFaqOpen}>
+        FAQ 불러오기
+      </button>
+      {isOpen && <FAQ onClick={onClick} />}
+    </ul>
   );
 };
