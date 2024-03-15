77070e7c4f2deed4c21853f01963408e692088df
designsoo <kheesoo10@gmail.com>
2024-03-16 03:15:17 +0900

    refactor: 별점 map index 수정(idx -> index)

--- src/components/commons/StarRating/index.tsx
+++ src/components/commons/StarRating/index.tsx
@@ -31,18 +31,18 @@ const StarRating = ({ size, rating, readonly = true }: StarRatingProps) => {
     <ul className={cx('star-rating')} aria-label={`${rating} out of 5`}>
       {Array(TOTAL_RATING)
         .fill(0)
-        .map((_, idx) => {
-          const filled = idx < selectedRating;
+        .map((_, index) => {
+          const filled = index < selectedRating;
           const { url, alt } = filled ? star.filled : star.empty;
 
           return (
-            <li key={`key-star-${idx}`}>
+            <li key={`key-star-${index}`}>
               {readonly ? (
                 <div className={cx(`star-size-${size}`)}>
                   <Image src={url} alt={alt} className={cx('star-icon')} fill></Image>
                 </div>
               ) : (
-                <button onClick={() => handleStarClick(idx)} className={cx(`star-size-${size}`)}>
+                <button onClick={() => handleStarClick(index)} className={cx(`star-size-${size}`)}>
                   <Image src={url} alt={alt} className={cx('star-icon')} fill></Image>
                 </button>
               )}
