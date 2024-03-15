c9dbcee0d580ca1140f852e6541c3a312bb008cd
grapefruit13 <liam.heo11@gmail.com>
2024-03-16 03:22:16 +0900

    feat: ImageField 구현, util 함수 분리

--- src/utils/toFixedOneDecimal.ts
+++ src/utils/toFixedOneDecimal.ts
@@ -3,3 +3,8 @@ const ONE_DECIMAL = 1;
 export const toFixedOneDecimal = (rating: number) => {
   return rating.toFixed(ONE_DECIMAL);
 };
+
+export const bytesToKilobytes = (bytes: number) => {
+  const kilobytes = bytes / 1024;
+  return kilobytes.toFixed(2);
+};
