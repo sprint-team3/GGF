c7343978413455c8657518ffcc1b5bb6a05c4694
ccwnc <ccwnc337@gmail.com>
2024-03-16 14:21:23 +0900

    chore: headerMockData 파일 추가

---
+++ src/constants/mockData/headerMockData.ts
@@ -0,0 +1,60 @@
+export const userData = {
+  id: 0,
+  email: 'test123@test.com',
+  nickname: 'test123',
+  profileImageUrl: '',
+  createdAt: '2024-03-15T14:21:31.180Z',
+  updatedAt: '2024-03-15T14:21:31.180Z',
+};
+
+export const alarmData = {
+  cursorId: 0,
+  notifications: [
+    {
+      id: 0,
+      teamId: 'string',
+      userId: 0,
+      content: '함께하면 즐거운 스트릿 댄스(2023-03-15 15:00~18:00) 예약이 승인되었어요.',
+      createdAt: '2024-03-15T14:25:59.858Z',
+      updatedAt: '2024-03-15T14:25:59.858Z',
+      deletedAt: '2024-03-15T14:25:59.858Z',
+    },
+    {
+      id: 1,
+      teamId: 'string',
+      userId: 0,
+      content: '함께하면 즐거운 스트릿 댄스(2023-03-15 15:00~18:00) 예약이 승인되었어요.',
+      createdAt: '2024-03-15T14:25:59.858Z',
+      updatedAt: '2024-03-15T14:25:59.858Z',
+      deletedAt: '2024-03-15T14:25:59.858Z',
+    },
+    {
+      id: 2,
+      teamId: 'string',
+      userId: 0,
+      content: '함께하면 즐거운 스트릿 댄스(2023-03-15 15:00~18:00) 예약이 승인되었어요.',
+      createdAt: '2024-03-15T14:25:59.858Z',
+      updatedAt: '2024-03-15T14:25:59.858Z',
+      deletedAt: '2024-03-15T14:25:59.858Z',
+    },
+    {
+      id: 3,
+      teamId: 'string',
+      userId: 0,
+      content: '함께하면 즐거운 스트릿 댄스(2023-03-15 15:00~18:00) 예약이 승인되었어요.',
+      createdAt: '2024-03-15T14:25:59.858Z',
+      updatedAt: '2024-03-15T14:25:59.858Z',
+      deletedAt: '2024-03-15T14:25:59.858Z',
+    },
+    {
+      id: 4,
+      teamId: 'string',
+      userId: 0,
+      content: '함께하면 즐거운 스트릿 댄스(2023-03-15 15:00~18:00) 예약이 승인되었어요.',
+      createdAt: '2024-03-15T14:25:59.858Z',
+      updatedAt: '2024-03-15T14:25:59.858Z',
+      deletedAt: '2024-03-15T14:25:59.858Z',
+    },
+  ],
+  totalCount: 5,
+};
