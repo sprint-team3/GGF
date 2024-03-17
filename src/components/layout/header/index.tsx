949ef371a2028720d0f2fba5d60dbdaa9174f2b7
ccwnc <ccwnc337@gmail.com>
2024-03-17 17:39:10 +0900

    refactor: deviceTpye 대신 css 속성 사용

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -14,7 +14,6 @@ import { HeaderProfile } from '@/components/layout/header/HeaderProfile';
 import { Menu } from '@/components/layout/header/Menu';
 import { UserMenu } from '@/components/layout/header/UserMenu';
 import { alarmData, userData } from '@/constants/mockData/headerMockData';
-import { useDeviceType } from '@/hooks/useDeviceType';
 import useTogglePopup from '@/hooks/useTogglePopup';
 
 import styles from './Header.module.scss';
@@ -23,7 +22,6 @@ const cx = classNames.bind(styles);
 const { url, alt } = SVGS.drawerMenu;
 
 export const Header = () => {
-  const deviceType = useDeviceType();
   const [isAlarmExisted, setIsAlarmExisted] = useState(false);
 
   const {
@@ -54,13 +52,23 @@ export const Header = () => {
     setIsAlarmExisted(totalCount > 0);
   }, [totalCount]);
 
-  return deviceType !== 'Mobile' ? (
+  return (
     <div className={cx('header')}>
+      <button className={cx('header-menu-button', 'sm-only')} ref={menuRef} onClick={handleToggleDrawerMenu}>
+        <Image src={url} alt={alt} width={24} height={24}></Image>
+      </button>
+      {isDrawerMenuOpened && (
+        <div className={cx('header-drawer-menu')}>
+          <DrawerMenu drawerMenuRef={drawerMenuRef} onClick={handleToggleDrawerMenu} />
+        </div>
+      )}
       <Link className={cx('header-logo')} href={'/'}>
         GGF
       </Link>
       <div className={cx('header-container-outer')}>
-        <Menu />
+        <div className={cx('sm-hidden')}>
+          <Menu />
+        </div>
         <div className={cx('header-container-inner')}>
           <Alarm
             isActivated={isAlarmActivated}
@@ -71,52 +79,11 @@ export const Header = () => {
           <HeaderProfile
             nickname={nickname}
             profileImageUrl={profileImageUrl}
-            deviceType={deviceType}
             isActivated={isHeaderProfileActivated}
             onClick={handleHeaderProfileActivation}
             headerProfileRef={headerProfileRef}
           />
         </div>
-      </div>
-      {isAlarmActivated && (
-        <div className={cx('header-alarm-list')}>
-          <AlarmList notifications={notifications} totalCount={totalCount} alarmListRef={alarmListRef} />
-        </div>
-      )}
-      {isHeaderProfileActivated && (
-        <div className={cx('header-user-menu')}>
-          <UserMenu profileImageUrl={profileImageUrl} nickname={nickname} email={email} userMenuRef={userMenuRef} />
-        </div>
-      )}
-    </div>
-  ) : (
-    <div className={cx('header')}>
-      <button className={cx('header-menu-button')} ref={menuRef} onClick={handleToggleDrawerMenu}>
-        <Image src={url} alt={alt} width={24} height={24}></Image>
-      </button>
-      {isDrawerMenuOpened && (
-        <div className={cx('header-drawer-menu')}>
-          <DrawerMenu drawerMenuRef={drawerMenuRef} onClick={handleToggleDrawerMenu} />
-        </div>
-      )}
-      <Link className={cx('header-logo', 'header-logo-mobile-size')} href={'/'}>
-        GGF
-      </Link>
-      <div className={cx('header-inner-container')}>
-        <Alarm
-          isActivated={isAlarmActivated}
-          isAlarmExisted={isAlarmExisted}
-          onClick={handleAlarmActivation}
-          alarmRef={alarmRef}
-        />
-        <HeaderProfile
-          nickname={nickname}
-          profileImageUrl={profileImageUrl}
-          deviceType={deviceType}
-          isActivated={isHeaderProfileActivated}
-          onClick={handleHeaderProfileActivation}
-          headerProfileRef={headerProfileRef}
-        />
       </div>
       {isAlarmActivated && (
         <div className={cx('header-alarm-list')}>
