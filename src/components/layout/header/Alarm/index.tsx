64ddefbb48fa316fa79ca4c2616a4225f1543058
ccwnc <ccwnc337@gmail.com>
2024-03-17 23:57:57 +0900

    refactor: Alarm 컴포넌트 리팩토링

--- src/components/layout/header/Alarm/index.tsx
+++ src/components/layout/header/Alarm/index.tsx
@@ -9,6 +9,7 @@ import { SVGS } from '@/constants';
 import styles from './Alarm.module.scss';
 
 const cx = classNames.bind(styles);
+const { full, empty } = SVGS.alarm;
 
 type AlarmProps = {
   isAlarmExisted: boolean;
@@ -17,24 +18,22 @@ type AlarmProps = {
   alarmRef: RefObject<HTMLButtonElement>;
 };
 
-export const Alarm = ({ isAlarmExisted, isActivated, onClick, alarmRef }: AlarmProps) => {
-  const { full, empty } = SVGS.alarm;
-
+const Alarm = ({ isAlarmExisted, isActivated, onClick, alarmRef }: AlarmProps) => {
   return (
-    <button onClick={onClick} ref={alarmRef}>
-      <div className={cx('outer-frame')}>
-        <div className={cx('dot', 'top', { 'is-activated': isActivated })}></div>
-        <div className={cx('dot', 'right', { 'is-activated': isActivated })}></div>
-        <div className={cx('dot', 'bottom', { 'is-activated': isActivated })}></div>
-        <div className={cx('dot', 'left', { 'is-activated': isActivated })}></div>
-        <div className={cx('inner-frame', { 'is-activated': isActivated })}>
-          {isAlarmExisted ? (
-            <Image src={full.url} alt={full.alt} width={20} height={20} />
-          ) : (
-            <Image src={empty.url} alt={empty.alt} width={20} height={20} />
-          )}
-        </div>
+    <button className={cx('frame-outer')} onClick={onClick} ref={alarmRef}>
+      <div className={cx('dot', 'dot-top', { 'dot-activated': isActivated })}></div>
+      <div className={cx('dot', 'dot-right', { 'dot-activated': isActivated })}></div>
+      <div className={cx('dot', 'dot-bottom', { 'dot-activated': isActivated })}></div>
+      <div className={cx('dot', 'dot-left', { 'dot-activated': isActivated })}></div>
+      <div className={cx('frame-inner', { 'frame-inner-activated': isActivated })}>
+        {isAlarmExisted ? (
+          <Image src={full.url} alt={full.alt} width={20} height={20} priority />
+        ) : (
+          <Image src={empty.url} alt={empty.alt} width={20} height={20} priority />
+        )}
       </div>
     </button>
   );
 };
+
+export default Alarm;
