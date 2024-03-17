d1a121691b5d6b0abea7f705a0302188bb756de5
grapefruit13 <liam.heo11@gmail.com>
2024-03-17 23:14:19 +0900

    refactor: try catch대신 onSuccess와 onError 사용

--- src/hooks/useSignin.ts
+++ src/hooks/useSignin.ts
@@ -9,26 +9,28 @@ import useUserStore from '@/stores/useUserStore';
 import { Account } from '@/types';
 
 const useSignin = (value: Account) => {
-  const { data, isError, mutate } = useMutation({
+  const { error, mutate } = useMutation({
     mutationFn: () => Auth.signin(value),
+    onSuccess(data) {
+      const { accessToken, refreshToken, user } = data.data;
+      Cookies.set('accessToken', accessToken, {
+        expires: ACCESS_TOKEN_EXPIRED_TIME,
+        secure: true,
+        sameSite: 'strict',
+      });
+      Cookies.set('refreshToken', refreshToken, {
+        expires: REFRESH_TOKEN_EXPIRED_TIME,
+        secure: true,
+        sameSite: 'strict',
+      });
+      useUserStore.setState({ user: user });
+    },
+    onError(error) {
+      console.error('useSignin', error);
+    },
   });
 
-  if (data) {
-    const { accessToken, refreshToken, user } = data.data;
-    Cookies.set('accessToken', accessToken, {
-      expires: ACCESS_TOKEN_EXPIRED_TIME,
-      secure: true,
-      sameSite: 'strict',
-    });
-    Cookies.set('refreshToken', refreshToken, {
-      expires: REFRESH_TOKEN_EXPIRED_TIME,
-      secure: true,
-      sameSite: 'strict',
-    });
-    useUserStore.setState({ user: user });
-  }
-
-  return { isError, mutate };
+  return { error, mutate };
 };
 
 export default useSignin;
