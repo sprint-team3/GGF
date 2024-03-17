92f87ab531479f82bbea93f0d7823fdf6252beb6
ccwnc <ccwnc337@gmail.com>
2024-03-17 17:39:10 +0900

    refactor: deviceTpye 대신 css 속성 사용

--- src/components/layout/header/HeaderProfile/index.tsx
+++ src/components/layout/header/HeaderProfile/index.tsx
@@ -8,8 +8,6 @@ import { SVGS } from '@/constants';
 
 import { Avatar } from '@/components/commons/Avatar';
 
-import { DeviceType } from '@/types';
-
 import styles from './HeaderProfile.module.scss';
 
 const cx = classNames.bind(styles);
@@ -19,7 +17,6 @@ const { top, bottom } = SVGS.arrow;
 type HeaderProfileProps = {
   nickname: string;
   profileImageUrl: string;
-  deviceType: DeviceType;
   isActivated: boolean;
   onClick: MouseEventHandler<HTMLButtonElement>;
   headerProfileRef: RefObject<HTMLButtonElement>;
@@ -28,7 +25,6 @@ type HeaderProfileProps = {
 export const HeaderProfile = ({
   nickname,
   profileImageUrl,
-  deviceType,
   isActivated,
   onClick,
   headerProfileRef,
@@ -38,16 +34,15 @@ export const HeaderProfile = ({
       <button className={cx('container')} onClick={onClick} ref={headerProfileRef}>
         <Avatar size='small' isActivated={isActivated} profileImageUrl={profileImageUrl} />
         <div className={cx('inner-container')}>
-          {deviceType === 'PC' && <p>{nickname}</p>}
-          {deviceType !== 'Mobile' && (
-            <Image
-              src={isActivated ? top.url : bottom.url}
-              alt={isActivated ? top.alt : bottom.alt}
-              width={16}
-              height={16}
-              priority
-            />
-          )}
+          <span className={cx('lg-only')}>{nickname}</span>
+          <Image
+            className={cx('sm-hidden')}
+            src={isActivated ? top.url : bottom.url}
+            alt={isActivated ? top.alt : bottom.alt}
+            width={16}
+            height={16}
+            priority
+          />
         </div>
       </button>
     </div>
