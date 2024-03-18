3193adc639e04c3a3cd78c8f6971c76adf959436
CheeseB <2489ckckck@naver.com>
2024-03-18 11:36:51 +0900

    refactor: 객체 키에 숫자를 넣어서 순서 유지할 수 있도록 변경

--- src/constants/games.ts
+++ src/constants/games.ts
@@ -1,12 +1,18 @@
-import { GameKey, GameName } from '@/types/games';
+import { GameKey, GameName } from '@/types';
 
-export const GAMES = {
-  'LEAGUE OF LEGENDS': '리그오브레전드',
-  BATTLEGROUNDS: '배틀그라운드',
-  'OVERWATCH 2': '오버워치 2',
-  MINECRAFT: '마인크래프트',
+export const GAME_KEY = {
+  0: 'LEAGUE OF LEGENDS',
+  1: 'BATTLEGROUNDS',
+  2: 'OVERWATCH 2',
+  3: 'MINECRAFT',
 } as const;
 
-export const GAME_KEY_LIST: GameKey[] = ['LEAGUE OF LEGENDS', 'BATTLEGROUNDS', 'OVERWATCH 2', 'MINECRAFT'];
+export const GAME_NAME = {
+  0: '리그오브레전드',
+  1: '배틀그라운드',
+  2: '오버워치 2',
+  3: '마인크래프트',
+} as const;
 
-export const GAME_NAME_LIST: GameName[] = ['리그오브레전드', '배틀그라운드', '오버워치 2', '마인크래프트'];
+export const GAME_KEY_LIST: GameKey[] = Object.values(GAME_KEY);
+export const GAME_NAME_LIST: GameName[] = Object.values(GAME_NAME);
--- src/types/games.ts
+++ src/types/games.ts
@@ -1,4 +1,4 @@
-import { GAMES } from '@/constants';
+import { GAME_KEY, GAME_NAME } from '@/constants';
 
-export type GameKey = keyof typeof GAMES;
-export type GameName = (typeof GAMES)[GameKey];
+export type GameKey = (typeof GAME_KEY)[keyof typeof GAME_KEY];
+export type GameName = (typeof GAME_NAME)[keyof typeof GAME_NAME];
