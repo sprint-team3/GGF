d9a2505ad836f593c8e3a17670f1db597ea482ad
CheeseB <2489ckckck@naver.com>
2024-03-18 11:09:21 +0900

    refactor: 컴포넌트 이름 및 폴더 변경

--- src/components/commons/cards/EmptyCard.tsx
+++ src/components/commons/cards/EmptyCard.tsx
@@ -4,22 +4,23 @@ import classNames from 'classnames/bind';
 
 import { SVGS } from '@/constants';
 
-import styles from './EmptyBox.module.scss';
+import styles from './EmptyCard.module.scss';
 
 const cx = classNames.bind(styles);
 
-type EmptyBoxProps = {
+type EmptyCardProps = {
   text: string;
 };
 
-const EmptyBox = ({ text }: EmptyBoxProps) => {
-  const { url, alt } = SVGS.empty;
+const { url, alt } = SVGS.empty;
+
+const EmptyCard = ({ text }: EmptyCardProps) => {
   return (
-    <div className={cx('empty-box')}>
+    <div className={cx('card-empty')}>
       <Image src={url} alt={alt} width={32} height={32} />
       <div>{text}</div>
     </div>
   );
 };
 
-export default EmptyBox;
+export default EmptyCard;
