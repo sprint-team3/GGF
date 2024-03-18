57fa10d4e4b642667e4c3759daffd809e43fde23
designsoo <kheesoo10@gmail.com>
2024-03-19 04:25:37 +0900

    feat: LandingPage 생성 및 레이아웃 컴포넌트 적용

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/layout/Layout';
+
+const LandingPage = () => {
+  return <div>LandingPage</div>;
+};
+
+export default LandingPage;
+
+LandingPage.FullLayout = Layout;
