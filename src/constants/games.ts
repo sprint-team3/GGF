ea84b6528f20d0c9e1bb6401e8121596a722b297
CheeseB <2489ckckck@naver.com>
2024-03-19 00:48:52 +0900

    refactor: 타입 정의 추가 및 수정

--- src/constants/games.ts
+++ src/constants/games.ts
@@ -1,18 +1,18 @@
 import { GameNameEN, GameNameKR } from '@/types';
 
-export const GAME_NAME_EN = {
-  0: 'LEAGUE OF LEGENDS',
-  1: 'BATTLEGROUNDS',
-  2: 'OVERWATCH 2',
-  3: 'MINECRAFT',
-} as const;
+export const GAME_NAME_EN_TO_KR: Record<GameNameEN, GameNameKR> = {
+  'LEAGUE OF LEGENDS': '리그오브레전드',
+  BATTLEGROUNDS: '배틀그라운드',
+  'OVERWATCH 2': '오버워치 2',
+  MINECRAFT: '마인크래프트',
+};
 
-export const GAME_NAME_KR = {
-  0: '리그오브레전드',
-  1: '배틀그라운드',
-  2: '오버워치 2',
-  3: '마인크래프트',
-} as const;
+export const CATEGORY_TO_GAME: Record<string, GameNameEN> = {
+  스포츠: 'LEAGUE OF LEGENDS',
+  투어: 'BATTLEGROUNDS',
+  관광: 'OVERWATCH 2',
+  웰빙: 'MINECRAFT',
+};
 
-export const GAME_NAME_LIST_EN: GameNameEN[] = Object.values(GAME_NAME_EN);
-export const GAME_NAME_LIST_KR: GameNameKR[] = Object.values(GAME_NAME_KR);
+export const GAME_NAME_LIST_EN: GameNameEN[] = ['LEAGUE OF LEGENDS', 'BATTLEGROUNDS', 'OVERWATCH 2', 'MINECRAFT'];
+export const GAME_NAME_LIST_KR: GameNameKR[] = ['리그오브레전드', '배틀그라운드', '오버워치 2', '마인크래프트'];
