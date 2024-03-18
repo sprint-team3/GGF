90c768b6b866d5c2332c72cc85470f34dde1a0fd
ccwnc <ccwnc337@gmail.com>
2024-03-18 23:23:34 +0900

    refactor: useMultipleTogglePopup 커스텀 훅 삭제

--- src/components/layout/header/Header/index.tsx
+++ src/components/layout/header/Header/index.tsx
@@ -14,7 +14,7 @@ import HeaderProfile from '@/components/layout/header/HeaderProfile';
 import Menu from '@/components/layout/header/Menu';
 import UserMenu from '@/components/layout/header/UserMenu';
 import { alarmData, userData } from '@/constants/mockData/headerMockData';
-import useMultipleTogglePopup from '@/hooks/useMultipleTogglePopup';
+import useTogglePopup from '@/hooks/useTogglePopup';
 
 import styles from './Header.module.scss';
 
@@ -23,7 +23,27 @@ const { url, alt } = SVGS.drawerMenu;
 
 const Header = () => {
   const [isAlarmExisted, setIsAlarmExisted] = useState(false);
-  const multipleTogglePopupResults = useMultipleTogglePopup(3);
+
+  const {
+    isOpen: isAlarmActivated,
+    popupRef: alarmListRef,
+    buttonRef: alarmRef,
+    togglePopup: handleAlarmActivation,
+  } = useTogglePopup();
+
+  const {
+    isOpen: isHeaderProfileActivated,
+    popupRef: userMenuRef,
+    buttonRef: headerProfileRef,
+    togglePopup: handleHeaderProfileActivation,
+  } = useTogglePopup();
+
+  const {
+    isOpen: isDrawerMenuActivated,
+    popupRef: drawerMenuRef,
+    buttonRef: menuRef,
+    togglePopup: handleDrawerMenuActivation,
+  } = useTogglePopup();
 
   const { email, nickname, profileImageUrl } = userData;
   const { totalCount, notifications } = alarmData;
@@ -35,11 +55,7 @@ const Header = () => {
   return (
     <div className={cx('container')}>
       <div className={cx('header')}>
-        <button
-          className={cx('header-menu-button', 'sm-only')}
-          ref={multipleTogglePopupResults[0].buttonRef}
-          onClick={multipleTogglePopupResults[0].togglePopup}
-        >
+        <button className={cx('header-menu-button', 'sm-only')} ref={menuRef} onClick={handleDrawerMenuActivation}>
           <Image src={url} alt={alt} width={24} height={24}></Image>
         </button>
         <Link className={cx('header-logo')} href={'/'}>
@@ -51,46 +67,34 @@ const Header = () => {
           </div>
           <div className={cx('header-container-inner')}>
             <Alarm
-              isActivated={multipleTogglePopupResults[1].isOpen}
+              isActivated={isAlarmActivated}
               isAlarmExisted={isAlarmExisted}
-              onClick={multipleTogglePopupResults[1].togglePopup}
-              alarmRef={multipleTogglePopupResults[1].buttonRef}
+              onClick={handleAlarmActivation}
+              alarmRef={alarmRef}
             />
             <HeaderProfile
               nickname={nickname}
               profileImageUrl={profileImageUrl}
-              isActivated={multipleTogglePopupResults[2].isOpen}
-              onClick={multipleTogglePopupResults[2].togglePopup}
-              headerProfileRef={multipleTogglePopupResults[2].buttonRef}
+              isActivated={isHeaderProfileActivated}
+              onClick={handleHeaderProfileActivation}
+              headerProfileRef={headerProfileRef}
             />
           </div>
         </div>
-        {multipleTogglePopupResults[1].isOpen && (
+        {isAlarmActivated && (
           <div className={cx('header-alarm-list')}>
-            <AlarmList
-              notifications={notifications}
-              totalCount={totalCount}
-              alarmListRef={multipleTogglePopupResults[1].popupRef}
-            />
+            <AlarmList notifications={notifications} totalCount={totalCount} alarmListRef={alarmListRef} />
           </div>
         )}
-        {multipleTogglePopupResults[2].isOpen && (
+        {isHeaderProfileActivated && (
           <div className={cx('header-user-menu')}>
-            <UserMenu
-              profileImageUrl={profileImageUrl}
-              nickname={nickname}
-              email={email}
-              userMenuRef={multipleTogglePopupResults[2].popupRef}
-            />
+            <UserMenu profileImageUrl={profileImageUrl} nickname={nickname} email={email} userMenuRef={userMenuRef} />
           </div>
         )}
       </div>
-      {multipleTogglePopupResults[0].isOpen && (
+      {isDrawerMenuActivated && (
         <div className={cx('header-drawer-menu')}>
-          <DrawerMenu
-            drawerMenuRef={multipleTogglePopupResults[0].popupRef}
-            onClick={multipleTogglePopupResults[0].togglePopup}
-          />
+          <DrawerMenu drawerMenuRef={drawerMenuRef} onClick={handleDrawerMenuActivation} />
         </div>
       )}
     </div>
