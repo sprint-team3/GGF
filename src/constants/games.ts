ff8fbb3a8e68895148dc3a270ddec7b24c2d78bf
CheeseB <2489ckckck@naver.com>
2024-03-19 01:17:19 +0900

    refactor: 불필요한 객체 삭제

--- src/constants/games.ts
+++ src/constants/games.ts
@@ -1,5 +1,7 @@
 import { GameNameEN, GameNameKR } from '@/types';
 
+export const GAME_NAME_LIST_EN: GameNameEN[] = ['LEAGUE OF LEGENDS', 'BATTLEGROUNDS', 'OVERWATCH 2', 'MINECRAFT'];
+
 export const GAME_NAME_EN_TO_KR: Record<GameNameEN, GameNameKR> = {
   'LEAGUE OF LEGENDS': '리그오브레전드',
   BATTLEGROUNDS: '배틀그라운드',
@@ -13,6 +15,3 @@ export const CATEGORY_TO_GAME: Record<string, GameNameEN> = {
   관광: 'OVERWATCH 2',
   웰빙: 'MINECRAFT',
 };
-
-export const GAME_NAME_LIST_EN: GameNameEN[] = ['LEAGUE OF LEGENDS', 'BATTLEGROUNDS', 'OVERWATCH 2', 'MINECRAFT'];
-export const GAME_NAME_LIST_KR: GameNameKR[] = ['리그오브레전드', '배틀그라운드', '오버워치 2', '마인크래프트'];
