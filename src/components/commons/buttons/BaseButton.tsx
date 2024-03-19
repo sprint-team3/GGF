dfc31a7e543a78fad60e7a46f391cb47cce9ebec
CheeseB <2489ckckck@naver.com>
2024-03-19 23:11:51 +0900

    hotfix: BaseButton에 type 추가

--- src/components/commons/buttons/BaseButton.tsx
+++ src/components/commons/buttons/BaseButton.tsx
@@ -13,13 +13,22 @@ type BaseButtonProps = {
   isDisabled?: boolean;
   onClick: MouseEventHandler<HTMLButtonElement>;
   color?: 'yellow' | 'purple' | 'red';
+  type?: 'button' | 'submit';
 };
 
-export const BaseButton = ({ theme, size, children, isDisabled, onClick, color = 'yellow' }: BaseButtonProps) => {
+export const BaseButton = ({
+  theme,
+  size,
+  children,
+  isDisabled,
+  onClick,
+  color = 'yellow',
+  type = 'button',
+}: BaseButtonProps) => {
   const buttonTheme = theme === 'outline' ? 'btn-theme-outline' : `btn-theme-${theme}-${color}`;
 
   return (
-    <button className={cx(`btn-size-${size}`, buttonTheme)} disabled={isDisabled} onClick={onClick}>
+    <button className={cx(`btn-size-${size}`, buttonTheme)} disabled={isDisabled} onClick={onClick} type={type}>
       <span>{children}</span>
     </button>
   );
