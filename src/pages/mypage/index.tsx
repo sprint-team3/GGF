cedcb7e43f44b38e0acd64b3413bcc15be377286
designsoo <kheesoo10@gmail.com>
2024-03-19 19:59:46 +0900

    feat: 마이페이지 컴포넌트 생성 및 레이아웃 적용

---
+++ src/pages/mypage/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/components/layout/Layout';
+import MypageContent from '@/components/mypage/MypageContent';
+
+const Mypage = () => {
+  return <MypageContent />;
+};
+
+export default Mypage;
+
+Mypage.FullLayout = Layout;
