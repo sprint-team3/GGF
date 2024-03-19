8e990b31beaf239d9eb275f42a0578c097b3f7f9
designsoo <kheesoo10@gmail.com>
2024-03-19 20:02:57 +0900

    feat: 마이페이지 등록 및 예약 현황 목업 데이터 세팅

---
+++ src/constants/mockData/profileSummaryMockData.ts
@@ -0,0 +1,47 @@
+export const MY_ACTIVITIES = {
+  cursorId: 0,
+  totalCount: 10,
+  activities: [
+    {
+      id: 0,
+      userId: 0,
+      title: 'string',
+      description: 'string',
+      category: 'string',
+      price: 0,
+      address: 'string',
+      bannerImageUrl: 'string',
+      rating: 0,
+      reviewCount: 0,
+      createdAt: '2024-03-19T06:28:52.888Z',
+      updatedAt: '2024-03-19T06:28:52.888Z',
+    },
+  ],
+};
+
+export const MY_RESERVATIONS = {
+  cursorId: 0,
+  reservations: [
+    {
+      id: 0,
+      teamId: 'string',
+      userId: 0,
+      activity: {
+        bannerImageUrl: 'string',
+        title: 'string',
+        id: 0,
+      },
+      scheduleId: 0,
+      status: 'pending',
+      reviewSubmitted: true,
+      totalPrice: 0,
+      headCount: 0,
+      date: 'string',
+      startTime: 'string',
+      endTime: 'string',
+      createdAt: '2024-03-19T06:31:49.862Z',
+      updatedAt: '2024-03-19T06:31:49.862Z',
+    },
+  ],
+  totalCount: 7,
+};
