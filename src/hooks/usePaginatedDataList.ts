063d1a0eda3b91595b99c79da14fc88ff205d07f
CheeseB <2489ckckck@naver.com>
2024-03-20 05:53:53 +0900

    feat: 데이터 페이징 적용하는 커스텀훅 추가

---
+++ src/hooks/usePaginatedDataList.ts
@@ -0,0 +1,27 @@
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+
+type Params<T> = {
+  initialDataList: T[];
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  postsPerPage: number;
+};
+
+const usePaginatedDataList = <T>({ initialDataList, page, setPage, postsPerPage }: Params<T>): T[] => {
+  const [pagedDataList, setPagedDataList] = useState<T[]>([]);
+
+  const startIndex = (page - 1) * postsPerPage;
+  const endIndex = page * postsPerPage;
+
+  useEffect(() => {
+    setPage(1);
+  }, [initialDataList, setPage]);
+
+  useEffect(() => {
+    setPagedDataList(initialDataList.filter((data, index) => index >= startIndex && index < endIndex));
+  }, [startIndex, endIndex, initialDataList]);
+
+  return pagedDataList;
+};
+
+export default usePaginatedDataList;
