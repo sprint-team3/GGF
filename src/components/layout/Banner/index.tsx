85e0e48554d4eb20aa16250a8b9df9db6d678ea6
ccwnc <ccwnc337@gmail.com>
2024-03-20 14:19:12 +0900

    refactor: Banner 컴포넌트 리팩토링

--- src/components/layout/Banner/index.tsx
+++ src/components/layout/Banner/index.tsx
@@ -1,23 +1,22 @@
 import classNames from 'classnames/bind';
 
-import { SVGS } from '@/constants';
+import { PNGS } from '@/constants';
 
 import styles from './Banner.module.scss';
 
 const cx = classNames.bind(styles);
 
 type BannerProps = {
-  gameName: keyof typeof SVGS.banner;
+  gameName: keyof typeof PNGS.banner;
 };
 
 const Banner = ({ gameName }: BannerProps) => {
-  const { url } = SVGS.banner[gameName];
+  const { url } = PNGS.banner[gameName];
 
   return (
     <section className={cx('banner')} style={{ backgroundImage: `url(${url})` }}>
       <div className={cx('banner-game')}>
         <h1 className={cx('banner-game-name')}>{gameName}</h1>
-        <hr className={cx('banner-game-line')} />
       </div>
     </section>
   );
