0adcac7248eabe1ec3a533b96055e57c78cf9499
grapefruit13 <liam.heo11@gmail.com>
2024-03-21 21:27:58 +0900

    feat: Account 페이지 구현

---
+++ src/pages/account/index.tsx
@@ -0,0 +1,10 @@
+import AccountForm from '@/components/AccountForm';
+import Layout from '@/components/layout/Layout';
+
+const AccountPage = () => {
+  return <AccountForm />;
+};
+
+export default AccountPage;
+
+AccountPage.FullLayout = Layout;
