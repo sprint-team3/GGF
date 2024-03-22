c618a1adfbbf28ecb32caf0bfbc78bfd3ebcad9b
designsoo <kheesoo10@gmail.com>
2024-03-23 03:51:40 +0900

    refactor: 포멧 변환 유틸 함수 파라미터 타입 변경

--- src/utils/localeFormatter.ts
+++ src/utils/localeFormatter.ts
@@ -1,7 +1,7 @@
-import { MY_RESERVATIONS_STATUS, POST_TYPES } from '@/constants';
+import { MY_RESERVATIONS_STATUS, POST_TYPES, PRICE_TO_POST_TYPES } from '@/constants';
 
-import { MyReservationsStatus, PostTypes } from '@/types';
+export const formatStatusToKR = (status: string) => MY_RESERVATIONS_STATUS[status];
 
-export const formatStatusToKR = (status: MyReservationsStatus) => MY_RESERVATIONS_STATUS[status];
+export const formatPostTypeToKR = (postType: string) => POST_TYPES[postType];
 
-export const formatPostTypeToKR = (postType: PostTypes) => POST_TYPES[postType];
+export const formatPriceToPostType = (price: number) => PRICE_TO_POST_TYPES[price];
