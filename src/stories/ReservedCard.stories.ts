da2d39e2c59f9a7154afa4a10052b9f7cb8dbc14
designsoo <kheesoo10@gmail.com>
2024-03-23 03:56:00 +0900

    refactor: 기존 파라미터 id가 reservationId로 변경된 내용 스토리북 적용

--- src/stories/ReservedCard.stories.ts
+++ src/stories/ReservedCard.stories.ts
@@ -12,14 +12,16 @@ type Story = StoryObj<typeof meta>;
 
 export const Example: Story = {
   args: {
+    reservationId: 0,
     path: '/',
     status: 'pending',
     postType: 'offline',
     title: '[PC방 대회]리그 오브 레전드 개발자들의 PC방 습격!',
     address: '강남 SBXG 포탈 PC방',
     category: '리그오브레전드',
-    createdAt: '2024-03-13T04:44:02.924Z',
     date: '2024-03-13',
+    startTime: '17:00',
     endTime: '18:00',
+    createdAt: '2024-03-13T04:44:02.924Z',
   },
 };
