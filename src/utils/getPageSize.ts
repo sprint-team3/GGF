77451058ef804a67d4a71451efe07c6cdfcbe7f1
CheeseB <2489ckckck@naver.com>
2024-03-22 12:00:45 +0900

    refactor: 상수명 PAGE_SIZE 를 DEFAULT_PAGE_SIZE 로 변경

--- src/utils/getPageSize.ts
+++ src/utils/getPageSize.ts
@@ -1,7 +1,9 @@
-import { PAGE_SIZE, POSTS_PER_PAGE, REVIEWS_PER_PAGE } from '@/constants';
+import { DEFAULT_PAGE_SIZE, POSTS_PER_PAGE, REVIEWS_PER_PAGE } from '@/constants';
 
 import { DeviceType } from '@/types';
 
-export const getPostPageSize = (deviceType: DeviceType) => (deviceType === 'Mobile' ? PAGE_SIZE : POSTS_PER_PAGE);
+export const getPostPageSize = (deviceType: DeviceType) =>
+  deviceType === 'Mobile' ? DEFAULT_PAGE_SIZE : POSTS_PER_PAGE;
 
-export const getReviewPageSize = (deviceType: DeviceType) => (deviceType === 'Mobile' ? PAGE_SIZE : REVIEWS_PER_PAGE);
+export const getReviewPageSize = (deviceType: DeviceType) =>
+  deviceType === 'Mobile' ? DEFAULT_PAGE_SIZE : REVIEWS_PER_PAGE;
