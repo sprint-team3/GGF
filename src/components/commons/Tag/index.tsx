7b3789bb5788b63b21f0cd61b1b796826e862590
designsoo <kheesoo10@gmail.com>
2024-03-23 03:43:01 +0900

    refactor: 예약된 모집 관련 재선언된 타입 별칭 적용

--- src/components/commons/Tag/index.tsx
+++ src/components/commons/Tag/index.tsx
@@ -2,14 +2,14 @@ import classNames from 'classnames/bind';
 
 import { formatPostTypeToKR } from '@/utils';
 
-import { PostTypes } from '@/types';
+import { ReservedPostTypesEN } from '@/types';
 
 import styles from './Tag.module.scss';
 
 const cx = classNames.bind(styles);
 
 type TagProps = {
-  postType: PostTypes;
+  postType: ReservedPostTypesEN | string;
 };
 
 const Tag = ({ postType }: TagProps) => {
