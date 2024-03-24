8ca4586ec05fe13ca5298bd0f10348efae037c2a
CheeseB <2489ckckck@naver.com>
2024-03-24 23:39:08 +0900

    feat: 예약 현황 스케줄 관련 타입 정의

--- src/types/schedules.ts
+++ src/types/schedules.ts
@@ -1,5 +1,33 @@
+import { ReservationResponse } from '@/types';
+
 export type AvailableSchedule = {
   date: string;
   startTime: string;
   endTime: string;
+};
+
+export type MonthlySchedule = {
+  date: string;
+  reservations: {
+    completed: number;
+    confirmed: number;
+    pending: number;
+  };
+};
+
+export type DailySchedule = {
+  scheduleId: number;
+  startTime: string;
+  endTime: string;
+  count: {
+    declined: number;
+    confirmed: number;
+    pending: number;
+  };
+};
+
+export type DetailSchedule = {
+  cursorId: 0;
+  totalCount: 0;
+  reservations: Omit<ReservationResponse, 'activity'>[];
 };
