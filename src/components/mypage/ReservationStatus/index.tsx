0f6e6d86107549befc1d976f6e0e7d29ae7b1b3b
CheeseB <2489ckckck@naver.com>
2024-03-24 22:41:08 +0900

    feat: 예약 현황 탭 기본 구조 작성

--- src/components/mypage/ReservationStatus/index.tsx
+++ src/components/mypage/ReservationStatus/index.tsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+
+import classNames from 'classnames/bind';
+
+import Calendar from '@/components/calendar';
+import Dropdown from '@/components/commons/Dropdown';
+import MockActivityDatas from '@/constants/mockData/myActivitiesMockData.json';
+// import MockScheduleDatas from '@/constants/mockData/myScheduleMockData.json';
+
+import styles from './ReservationStatus.module.scss';
+
+const cx = classNames.bind(styles);
+
+const postDropdownOptions = MockActivityDatas.map(({ id, title }) => ({ title, value: id }));
+
 const ReservationStatus = () => {
-  return <div>ReservationStatus</div>;
+  const [gameId, setGameId] = useState(postDropdownOptions[0].value);
+
+  const handlePostChange = (value: number | string) => {
+    setGameId(value as number);
+  };
+
+  console.log(gameId);
+
+  return (
+    <div className={cx('.status-container')}>
+      <Dropdown options={postDropdownOptions} onChange={handlePostChange} label='모집명' color='yellow' />
+      <Calendar />
+    </div>
+  );
 };
 
 export default ReservationStatus;
