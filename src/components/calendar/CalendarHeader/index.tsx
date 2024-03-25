b09ad0d126bd77be0f5ce14d32f4d5717afcb895
CheeseB <2489ckckck@naver.com>
2024-03-26 04:04:14 +0900

    feat: 달력 헤더부분 구현

---
+++ src/components/calendar/CalendarHeader/index.tsx
@@ -0,0 +1,45 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import classNames from 'classnames/bind';
+
+import SwitchButton from '@/components/calendar/SwitchButton';
+import { ArrowButton } from '@/components/commons/buttons';
+import { MONTHS } from '@/constants/date';
+
+import styles from './CalendarHeader.module.scss';
+
+const cx = classNames.bind(styles);
+
+type CalendarHeaderProps = {
+  currentYear: number;
+  currentMonth: number;
+  onChangeMonth: (addNumber: number) => void;
+  isCalendar: boolean;
+  setIsCalendar: Dispatch<SetStateAction<boolean>>;
+};
+
+const CalendarHeader = ({
+  currentYear,
+  currentMonth,
+  onChangeMonth,
+  isCalendar,
+  setIsCalendar,
+}: CalendarHeaderProps) => {
+  const handleSwitchClick = (isCalendar: boolean) => setIsCalendar(isCalendar);
+
+  return (
+    <div className={cx('calendar-header-container')}>
+      <div className={cx('arrow-area')}>
+        <ArrowButton onClick={onChangeMonth} />
+      </div>
+      <div className={cx('calendar-header-date')}>
+        {MONTHS[currentMonth]} <span className={cx('calendar-header-year')}>{currentYear}</span>
+      </div>
+      <div className={cx('lg-only')}>
+        <SwitchButton isCalendar={isCalendar} onClick={handleSwitchClick} />
+      </div>
+    </div>
+  );
+};
+
+export default CalendarHeader;
