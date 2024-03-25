bc3eb56813b763e8eb447be329259257c09e8309
grapefruit13 <liam.heo11@gmail.com>
2024-03-25 13:16:05 +0900

    refactor: sort 옵션 띄어쓰기 수정

--- src/constants/sortOptions.ts
+++ src/constants/sortOptions.ts
@@ -2,10 +2,10 @@ import { Order } from '@/types';
 
 export const SORT_OPTIONS = [
   { title: '최신순', value: 'desc' as Order },
-  { title: '오래된순', value: 'asc' as Order },
+  { title: '오래된 순', value: 'asc' as Order },
 ];
 
 export const REVIEW_SORT_OPTIONS = [
-  { title: '평점 높은순', value: 'desc' as Order },
-  { title: '평점 낮은순', value: 'asc' as Order },
+  { title: '평점 높은 순', value: 'desc' as Order },
+  { title: '평점 낮은 순', value: 'asc' as Order },
 ];
