b676bf524da8794baa0b690cfb7a19e8027f5083
CheeseB <2489ckckck@naver.com>
2024-03-26 04:05:21 +0900

    feat: 캘린터 틀 구성

---
+++ src/components/calendar/CalendarBody/index.tsx
@@ -0,0 +1,11 @@
+import classNames from 'classnames/bind';
+
+import styles from './CalendarBody.module.scss';
+
+const cx = classNames.bind(styles);
+
+const CalendarBody = () => {
+  return <div className={cx('')}>CalendarBody</div>;
+};
+
+export default CalendarBody;
