c7f1c79621fdbcd220f6b1d7f3ab9c1c9ee03aea
CheeseB <2489ckckck@naver.com>
2024-03-26 09:21:15 +0900

    feat: 캘린더 요일 추가

--- src/components/calendar/CalendarBody/index.tsx
+++ src/components/calendar/CalendarBody/index.tsx
@@ -1,11 +1,29 @@
 import classNames from 'classnames/bind';
 
+import { ONE_WEEK } from '@/constants/date';
+
 import styles from './CalendarBody.module.scss';
 
 const cx = classNames.bind(styles);
 
+const NUM_OF_WEEKDAY = 5;
+
 const CalendarBody = () => {
-  return <div className={cx('')}>CalendarBody</div>;
+  return (
+    <div className={cx('calendar-body-container')}>
+      <ul className={cx('calendar-body-week')}>
+        {ONE_WEEK.map((day, index) => {
+          const isWeekends = index >= NUM_OF_WEEKDAY;
+
+          return (
+            <li className={cx('calendar-body-day', { weekends: isWeekends })} key={`day-${index}`}>
+              {day}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default CalendarBody;
