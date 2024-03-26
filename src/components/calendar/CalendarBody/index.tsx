3af2641af404cba84fa5706c5a4749d67b34e592
CheeseB <2489ckckck@naver.com>
2024-03-26 21:49:18 +0900

    feat: 모킹 데이터로 캘린더 스케줄 표시 구현

--- src/components/calendar/CalendarBody/index.tsx
+++ src/components/calendar/CalendarBody/index.tsx
@@ -1,9 +1,11 @@
 import classNames from 'classnames/bind';
 
-import { getCalendarDates, getDateRange } from '@/utils';
+import { getCalendarDates, getDateRange, getJoinedDateString } from '@/utils';
 
 import CalendarItem from '@/components/calendar/CalendarItem';
 import { ONE_WEEK } from '@/constants/date';
+
+import { ParsedMonthlySchedule } from '@/types';
 
 import styles from './CalendarBody.module.scss';
 
@@ -16,13 +18,14 @@ type CalendarBodyProps = {
   today: {
     year: number;
     month: number;
-    day: number;
+    date: number;
   };
   currentYear: number;
   currentMonth: number;
+  schedules?: ParsedMonthlySchedule;
 };
 
-const CalendarBody = ({ today, currentYear, currentMonth }: CalendarBodyProps) => {
+const CalendarBody = ({ today, currentYear, currentMonth, schedules }: CalendarBodyProps) => {
   const { startOfCalendar, endOfPrevMonth, endOfThisMonth, endOfCalendar } = getCalendarDates(
     currentYear,
     currentMonth,
@@ -32,7 +35,7 @@ const CalendarBody = ({ today, currentYear, currentMonth }: CalendarBodyProps) =
   const thisMonthDates = getDateRange(START_OF_MONTH, endOfThisMonth);
   const nextMonthDates = endOfCalendar !== endOfThisMonth ? getDateRange(START_OF_MONTH, endOfCalendar) : [];
 
-  const isToday = (date: number) => currentYear === today.year && currentMonth === today.month && date === today.day;
+  const isToday = (date: number) => currentYear === today.year && currentMonth === today.month && date === today.date;
 
   return (
     <div className={cx('calendar-body-container')}>
@@ -53,11 +56,15 @@ const CalendarBody = ({ today, currentYear, currentMonth }: CalendarBodyProps) =
             <CalendarItem date={date} isDisabled />
           </div>
         ))}
-        {thisMonthDates.map((date, index) => (
-          <div className={cx('date-item', 'hover')} key={`date-${index}`}>
-            <CalendarItem date={date} isToday={isToday(date)} />
-          </div>
-        ))}
+        {thisMonthDates.map((date, index) => {
+          const formattedDate = getJoinedDateString(currentYear, currentMonth, date);
+
+          return (
+            <div className={cx('date-item', 'hover')} key={`date-${index}`}>
+              <CalendarItem date={date} isToday={isToday(date)} reservations={schedules?.[formattedDate]} />
+            </div>
+          );
+        })}
         {nextMonthDates.map((date, index) => (
           <div className={cx('date-item')} key={`date-next-${index}`}>
             <CalendarItem date={date} isDisabled />
