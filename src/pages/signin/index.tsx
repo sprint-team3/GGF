4dbc410fb04b6fd8edc4884b9f40c500b328f720
grapefruit13 <liam.heo11@gmail.com>
2024-03-28 16:35:47 +0900

    feat: 로그인상태로 로그인페이지 접근시 리다이렉트 로직 추가

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,4 +1,25 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { getAuthCookie } from '@/utils';
+
 import SigninForm from '@/components/auth/SigninForm';
+
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  const { accessToken } = getAuthCookie(context);
+
+  if (accessToken) {
+    return {
+      redirect: {
+        destination: `/league-of-legends`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const SigninPage = () => {
   return <SigninForm />;
