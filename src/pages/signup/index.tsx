da3bf892a30023511ba02ea528e1363846fa0381
grapefruit13 <liam.heo11@gmail.com>
2024-03-28 20:36:01 +0900

    feat: 로그인 상태로 회원가입 페이지 접근 시 리다이렉트

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,4 +1,25 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { getAuthCookie } from '@/utils';
+
 import SignupForm from '@/components/auth/SignupForm';
+
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  const { accessToken } = getAuthCookie(context);
+
+  if (accessToken) {
+    return {
+      redirect: {
+        destination: `/league-of-legends`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const SignupPage = () => {
   return <SignupForm />;
