d49c1e1d68f9305be3ea02e0425e116b1dbbe5ce
CheeseB <2489ckckck@naver.com>
2024-03-28 17:17:02 +0900

    feat: 예약 데이터 없을때 EmptyCard 보이도록 수정

--- src/components/layout/empty/EmptyCard/index.tsx
+++ src/components/layout/empty/EmptyCard/index.tsx
@@ -10,13 +10,14 @@ const cx = classNames.bind(styles);
 
 type EmptyCardProps = {
   text: string;
+  isSmall?: boolean;
 };
 
 const { url, alt } = SVGS.empty;
 
-const EmptyCard = ({ text }: EmptyCardProps) => {
+const EmptyCard = ({ text, isSmall }: EmptyCardProps) => {
   return (
-    <div className={cx('card-empty')}>
+    <div className={cx('card-empty', { small: isSmall })}>
       <Image src={url} alt={alt} width={32} height={32} />
       <div>{text}</div>
     </div>
