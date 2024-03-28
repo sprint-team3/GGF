e40831f0ba4fd00d06ccfa05cc60a0c351089c52
CheeseB <2489ckckck@naver.com>
2024-03-28 11:02:28 +0900

    feat: 예약현황 모달에 탭 추가

--- src/components/calendar/ModalContents/index.tsx
+++ src/components/calendar/ModalContents/index.tsx
@@ -1,4 +1,12 @@
+import { useState } from 'react';
+
 import classNames from 'classnames/bind';
+
+import { SCHEDULE_TAB_OPTIONS } from '@/constants';
+
+import Tab from '@/components/commons/Tab';
+
+import { MyReservationsStatus } from '@/types';
 
 import styles from './ModalContents.module.scss';
 
@@ -10,9 +18,19 @@ type ModalContentsProps = {
 };
 
 const ModalContents = ({ gameId, activeDate }: ModalContentsProps) => {
+  const [selectedTabId, setSelectedTabId] = useState<MyReservationsStatus>(SCHEDULE_TAB_OPTIONS[0].id);
+
+  console.log(gameId);
+  console.log(activeDate);
+
   return (
-    <div className={cx('')}>
-      {gameId} / {activeDate}
+    <div className={cx('schedule-modal-container')}>
+      <Tab
+        items={SCHEDULE_TAB_OPTIONS}
+        size='small'
+        selectedTabId={selectedTabId}
+        onClick={(selectedId) => setSelectedTabId(selectedId as MyReservationsStatus)}
+      />
     </div>
   );
 };
