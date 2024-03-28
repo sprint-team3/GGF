8a5e16302bffa5ed666b488d720127324a0a8150
ccwnc <ccwnc337@gmail.com>
2024-03-28 21:01:22 +0900

    refactor: 이미지가 없을 시 기본 이미지를 동적으로 추가해주는 로직 구현

--- src/utils/gameFormatter.ts
+++ src/utils/gameFormatter.ts
@@ -1,4 +1,4 @@
-import { CATEGORY_TO_GAME, GAME_NAME_EN_TO_KR } from '@/constants';
+import { CATEGORY_TO_GAME, GAME_NAME_EN_TO_KR, PNGS } from '@/constants';
 
 import { GameNameEN, LinkName } from '@/types';
 
@@ -9,3 +9,6 @@ export const formatLinkToGame = (link: LinkName) => link.toUpperCase().replace(/
 export const formatCategoryToGameNameEN = (category: string) => CATEGORY_TO_GAME[category];
 
 export const formatCategoryToGameNameKR = (category: string) => GAME_NAME_EN_TO_KR[CATEGORY_TO_GAME[category]];
+
+export const formatCategoryToBannerImageURL = (category: string) =>
+  PNGS.banner[formatCategoryToGameNameEN(category)].url;
