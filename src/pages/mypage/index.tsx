7c206f34b399fe5b6e4c8347fd8c4a1f73e2b8a5
designsoo <kheesoo10@gmail.com>
2024-03-28 22:36:21 +0900

    feat: 리액트 쿼리 SSR 적용 및 토큰 없을 경우 landing 페이지 리다이렉트 설정

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,5 +1,33 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { dehydrate, QueryClient } from '@tanstack/react-query';
+
+import { getMyReservations } from '@/apis/queryFunctions';
+import { QUERY_KEYS } from '@/apis/queryKeys';
+import { PAGE_PATHS } from '@/constants';
+import { getAuthCookie, requiresLogin, setAuthCookie } from '@/utils';
+
 import Layout from '@/components/layout/Layout';
 import MypageContent from '@/components/mypage/MypageContent';
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { accessToken, refreshToken } = getAuthCookie(context);
+
+  const res = await requiresLogin(context, accessToken, refreshToken, PAGE_PATHS.landing);
+  if (res) {
+    const { newAccessToken, newRefreshToken } = res;
+    setAuthCookie(context, newAccessToken, newRefreshToken);
+  }
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({ queryKey: QUERY_KEYS.myReservations.get, queryFn: getMyReservations });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+}
 
 const Mypage = () => {
   return <MypageContent />;
