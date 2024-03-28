936494ac3abf622db156ef2b83b6bc27c11e0ff5
grapefruit13 <liam.heo11@gmail.com>
2024-03-28 16:41:51 +0900

    feat: account 페이지 api 연결

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,6 +1,24 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { PAGE_PATHS } from '@/constants';
+import { getAuthCookie, requiresLogin, setAuthCookie } from '@/utils';
+
 import AccountForm from '@/components/AccountForm';
 import Layout from '@/components/layout/Layout';
 
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { accessToken, refreshToken } = getAuthCookie(context);
+
+  const res = await requiresLogin(context, accessToken, refreshToken, PAGE_PATHS.mainList);
+  if (res) {
+    const { newAccessToken, newRefreshToken } = res;
+    setAuthCookie(context, newAccessToken, newRefreshToken);
+  }
+
+  return {
+    props: {},
+  };
+}
 const AccountPage = () => {
   return <AccountForm />;
 };
