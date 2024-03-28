f2243d192f55adeddc2972a6ca5d6f0aa6c9e7c2
grapefruit13 <liam.heo11@gmail.com>
2024-03-29 00:10:08 +0900

    feat: 모집 상세 페이지 SSR 리액트쿼리 데이터 요청

--- src/pages/[game]/[postId]/index.tsx
+++ src/pages/[game]/[postId]/index.tsx
@@ -1,8 +1,55 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { dehydrate } from '@tanstack/react-query';
+
+import { getActivityDetail, getReviewList } from '@/apis/queryFunctions';
+import { QUERY_KEYS } from '@/apis/queryKeys';
+import { getAuthCookie, queryClient } from '@/utils';
+
 import Layout from '@/components/layout/Layout';
 import PostConent from '@/components/postDetail/PostDetailContent';
 
-const PostPage = () => {
-  return <PostConent />;
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  const { accessToken } = getAuthCookie(context);
+  const isLoggedIn = !!accessToken;
+
+  const { params } = context;
+  const postId = Number(params?.postId);
+  const game = params?.game;
+
+  if (!postId) {
+    return {
+      redirect: {
+        destination: `/${game}`,
+        permanent: false,
+      },
+    };
+  }
+
+  queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS.activities.get, postId],
+    queryFn: getActivityDetail,
+  });
+
+  queryClient.prefetchQuery({
+    queryKey: [QUERY_KEYS.activities.getReviewList, postId],
+    queryFn: getReviewList,
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      isLoggedIn,
+    },
+  };
+}
+
+export type PostPageProps = {
+  isLoggedIn: boolean;
+};
+
+const PostPage = ({ isLoggedIn }: PostPageProps) => {
+  return <PostConent isLoggedIn={isLoggedIn} />;
 };
 
 export default PostPage;
