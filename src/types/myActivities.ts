8521d6fd4b53234a4cb90b2e70ed7438f4df5859
grapefruit13 <liam.heo11@gmail.com>
2024-03-29 00:05:42 +0900

    feat: ActivityDetailResponse 타입 추가

--- src/types/myActivities.ts
+++ src/types/myActivities.ts
@@ -40,6 +40,23 @@ export type ActivityResponse = {
   updatedAt: string;
 };
 
+export type ActivityDetailResponse = {
+  id: number;
+  userId: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  address: string;
+  bannerImageUrl: string;
+  subImages: { id: number; imageUrl: string }[];
+  schedules: { id: number; date: string; startTime: string; endTime: string }[];
+  reviewCount: number;
+  rating: number;
+  createdAt: string;
+  updatedAt: string;
+};
+
 export type ReviewResponse = {
   averageRating: number;
   totalCount: number;
