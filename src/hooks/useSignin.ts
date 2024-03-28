f01b31f955ff21e27599bd869720f42ca2895bc0
grapefruit13 <liam.heo11@gmail.com>
2024-03-28 16:34:12 +0900

    refactor: useSignin 훅 토큰 저장 로직 및 파람 수정

--- src/hooks/useSignin.ts
+++ src/hooks/useSignin.ts
@@ -1,28 +1,18 @@
 import { useMutation } from '@tanstack/react-query';
-import Cookies from 'js-cookie';
 
 import { Auth } from '@/apis/auth';
-import { ACCESS_TOKEN_EXPIRED_TIME, REFRESH_TOKEN_EXPIRED_TIME } from '@/constants';
+import { setAuthCookie } from '@/utils';
 
 import useUserStore from '@/stores/useUserStore';
 
 import { Account } from '@/types';
 
-const useSignin = (value: Account) => {
+const useSignin = () => {
   const { error, mutate } = useMutation({
-    mutationFn: () => Auth.signin(value),
+    mutationFn: (value: Account) => Auth.signin(value),
     onSuccess(data) {
       const { accessToken, refreshToken, user } = data.data;
-      Cookies.set('accessToken', accessToken, {
-        expires: ACCESS_TOKEN_EXPIRED_TIME,
-        secure: true,
-        sameSite: 'strict',
-      });
-      Cookies.set('refreshToken', refreshToken, {
-        expires: REFRESH_TOKEN_EXPIRED_TIME,
-        secure: true,
-        sameSite: 'strict',
-      });
+      setAuthCookie(null, accessToken, refreshToken);
       useUserStore.setState({ user: user });
     },
   });
