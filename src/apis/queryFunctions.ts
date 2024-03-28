9c21b34af5211426645584e58611bbc5e146e24a
grapefruit13 <liam.heo11@gmail.com>
2024-03-29 00:04:31 +0900

    feat: getActivityDetail, getReviewList query func 구현

--- src/apis/queryFunctions.ts
+++ src/apis/queryFunctions.ts
@@ -1,6 +1,24 @@
+import { QueryFunction } from '@tanstack/react-query';
+
 import { MyReservations } from '@/apis/myReservations';
+
+import { ActivityDetailResponse, ReviewResponse } from '@/types';
+
+import Activities from './activities';
 
 export const getMyReservations = async () => {
   const response = await MyReservations.get();
   return response.data.reservations;
+};
+
+export const getActivityDetail: QueryFunction<ActivityDetailResponse, [string, number]> = async ({ queryKey }) => {
+  const postId = queryKey[1];
+  const response = await Activities.get(postId);
+  return response.data;
+};
+
+export const getReviewList: QueryFunction<ReviewResponse, [string, number]> = async ({ queryKey }) => {
+  const postId = queryKey[1];
+  const response = await Activities.getReviewList(postId);
+  return response.data;
 };
