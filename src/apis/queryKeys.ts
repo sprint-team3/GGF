5fa0bc5c6d03a1647691218cdec8cb889dfd60da
CheeseB <2489ckckck@naver.com>
2024-03-29 10:05:39 +0900

    feat: myActivities 쿼리키 추가

--- src/apis/queryKeys.ts
+++ src/apis/queryKeys.ts
@@ -1,12 +1,32 @@
+import { ReservationStatus } from '@/types';
+
 export const QUERY_KEYS = {
   activities: {
     get: 'activityDetail',
     getSchedules: (activityId: number) => ['getSchedules', activityId],
     getReviewList: 'reviewList',
   },
+
   myReservations: {
     get: ['myReservation'],
     cancel: (reservationId: number) => ['myReservation', reservationId],
     createReview: (reservationId: number) => ['createReview', reservationId],
+  },
+
+  myActivities: {
+    getList: ['myActivities'],
+    getMonthlyReservationList: (activityId: number, year: string, month: string) => [
+      'myActivities',
+      activityId,
+      year,
+      month,
+    ],
+    getDailyReservationList: (activityId: number, date: string) => ['myActivities', activityId, date],
+    getHourlyReservationList: (activityId: number, scheduleId: number, status: ReservationStatus) => [
+      'myActivities',
+      activityId,
+      scheduleId,
+      status,
+    ],
   },
 };
