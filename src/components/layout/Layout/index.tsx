bbd2372ce8b5eaf2d1892722176e4cee937d11ee
grapefruit13 <liam.heo11@gmail.com>
2024-03-30 02:38:10 +0900

    refactor: layout에서 유저, 알림 데이터 요청 후 store에 저장

--- src/components/layout/Layout/index.tsx
+++ src/components/layout/Layout/index.tsx
@@ -1,9 +1,14 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 
 import classNames from 'classnames/bind';
 
+import { getCSRCookie } from '@/utils';
+
 import { Chatbot } from '@/components/commons/chatbot';
 import Header from '@/components/layout/header/Header';
+import useAlarmData from '@/hooks/useAlarmData';
+import useUserData from '@/hooks/useUserData';
+import useUserStore from '@/stores/useUserStore';
 
 import styles from './Layout.module.scss';
 
@@ -14,10 +19,20 @@ type LayoutProps = {
 };
 
 const Layout = ({ children }: LayoutProps) => {
+  const { accessToken } = getCSRCookie();
+  const { setUserData } = useUserStore();
+
+  const { userData } = useUserData(accessToken);
+  const { alarmData } = useAlarmData(accessToken);
+
+  useEffect(() => {
+    setUserData(userData);
+  }, [userData]);
+
   return (
     <>
       <div className={cx('header')}>
-        <Header />
+        <Header userData={userData} alarmData={alarmData} accessToken={accessToken} />
       </div>
       <div className={cx('content')}>
         {children}
