27f9c524e848f1b26a08a0f07455b690fb9e5989
CheeseB <2489ckckck@naver.com>
2024-03-29 10:26:05 +0900

    myActivities 쿼리 함수 추가

--- src/apis/queryFunctions.ts
+++ src/apis/queryFunctions.ts
@@ -1,8 +1,15 @@
 import { QueryFunction } from '@tanstack/react-query';
 
+import { MyActivities } from '@/apis/myActivities';
 import { MyReservations } from '@/apis/myReservations';
 
-import { ActivityDetailResponse, ReviewResponse } from '@/types';
+import {
+  ActivityDetailResponse,
+  ReviewResponse,
+  EditReservationStatusBody,
+  MyActivitiesBody,
+  ReservationStatus,
+} from '@/types';
 
 import Activities from './activities';
 
@@ -21,4 +28,47 @@ export const getReviewList: QueryFunction<ReviewResponse, [string, number]> = as
   const postId = queryKey[1];
   const response = await Activities.getReviewList(postId);
   return response.data;
+};
+
+export const getMyActivitiesList = async () => {
+  const response = await MyActivities.getList();
+  return response.data;
+};
+
+export const getMyActivitiesMonthlyReservationList = async (activityId: number, year: string, month: string) => {
+  const response = await MyActivities.getMonthlyReservationList(activityId, year, month);
+  return response.data;
+};
+
+export const getMyActivitiesDailyReservationList = async (activityId: number, date: string) => {
+  const response = await MyActivities.getDailyReservationList(activityId, date);
+  return response.data;
+};
+
+export const getMyActivitiesHourlyReservationList = async (
+  activityId: number,
+  scheduleId: number,
+  status: ReservationStatus,
+) => {
+  const response = await MyActivities.getHourlyReservationList(activityId, scheduleId, status);
+  return response.data;
+};
+
+export const editMyActivitiesReservationStatus = async (
+  activityId: number,
+  reservationId: number,
+  status: EditReservationStatusBody,
+) => {
+  const response = await MyActivities.editReservationStatus(activityId, reservationId, status);
+  return response.status;
+};
+
+export const deleteMyActivities = async (activityId: number) => {
+  const response = await MyActivities.delete(activityId);
+  return response.status;
+};
+
+export const editMyActivities = async (activityId: number, myActivities: MyActivitiesBody) => {
+  const response = await MyActivities.edit(activityId, myActivities);
+  return response.status;
 };
