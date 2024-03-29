4396e194c710943c089a13d150b731c38d2ddd79
CheeseB <2489ckckck@naver.com>
2024-03-29 16:57:39 +0900

    feat: 캘린더 드롭다운에 api 데이터 적용

--- src/components/mypage/ReservationStatus/index.tsx
+++ src/components/mypage/ReservationStatus/index.tsx
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 
+import { useQuery } from '@tanstack/react-query';
 import classNames from 'classnames/bind';
+
+import { getMyActivitiesList } from '@/apis/queryFunctions';
+import { QUERY_KEYS } from '@/apis/queryKeys';
+import { splitTitleByDelimiter } from '@/utils';
 
 import Calendar from '@/components/calendar';
 import Dropdown from '@/components/commons/Dropdown';
-import MockActivityDatas from '@/constants/mockData/myActivitiesMockData.json';
+
+import { MyActivitiesResponse } from '@/types';
 
 import styles from './ReservationStatus.module.scss';
 
 const cx = classNames.bind(styles);
 
 const ReservationStatus = () => {
-  const postDropdownOptions = MockActivityDatas.map(({ id, title }) => ({ title, value: id }));
+  const { data: myActivityDatas } = useQuery({
+    queryKey: QUERY_KEYS.myActivities.getList,
+    queryFn: getMyActivitiesList,
+  });
+
+  const postDropdownOptions = myActivityDatas.map((data: MyActivitiesResponse) => ({
+    title: splitTitleByDelimiter(data.title).title,
+    value: data.id,
+  }));
 
   const [gameId, setGameId] = useState(postDropdownOptions[0].value);
 
