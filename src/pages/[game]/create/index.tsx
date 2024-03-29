53d7748560aef4c93063936186d9090f902b58be
ccwnc <ccwnc337@gmail.com>
2024-03-30 02:45:23 +0900

    feat: 등록 페이지 API 연동

--- src/pages/[game]/create/index.tsx
+++ src/pages/[game]/create/index.tsx
@@ -1,8 +1,44 @@
+import { GetServerSidePropsContext } from 'next';
+
+import { GAME_T0_CATEGORY, PAGE_PATHS } from '@/constants';
+import { formatLinkToGame, getAuthCookie, isValidGameName } from '@/utils';
+
 import CreatePageContent from '@/components/createPage/CreatePageContent';
 import Layout from '@/components/layout/Layout';
 
-const CreatePage = () => {
-  return <CreatePageContent />;
+import { Category, GameNameEN, LinkName } from '@/types';
+
+export function getServerSideProps(context: GetServerSidePropsContext) {
+  const { accessToken } = getAuthCookie(context);
+  const isLoggedIn = !!accessToken;
+
+  const game = context.params?.game;
+  const isValid = isValidGameName(game as string);
+
+  if (!isLoggedIn || !isValid) {
+    return {
+      redirect: {
+        destination: PAGE_PATHS.signin,
+        permanent: false,
+      },
+    };
+  }
+
+  const gameName = formatLinkToGame(game as LinkName) as GameNameEN;
+  const category = GAME_T0_CATEGORY[gameName];
+
+  return {
+    props: { gameName, category },
+  };
+}
+
+export type CreatePageContentProps = {
+  gameName: GameNameEN;
+  category: Category;
+};
+
+const CreatePage = ({ gameName, category }: CreatePageContentProps) => {
+  return <CreatePageContent gameName={gameName} category={category} />;
 };
 
 export default CreatePage;
