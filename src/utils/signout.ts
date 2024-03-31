334fb66d32c161472f5b96db461c96d8c1656117
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 02:30:32 +0900

    refactor: signout함수에서 userData 초기값으로 세팅하도록 변경

--- src/utils/signout.ts
+++ src/utils/signout.ts
@@ -1,9 +1,11 @@
 import { destroyCookie } from 'nookies';
 
+import { USER_INITIAL_DATA } from '@/constants';
+
 import useUserStore from '@/stores/useUserStore';
 
 export const signout = () => {
   destroyCookie(null, 'accessToken');
   destroyCookie(null, 'refreshToken');
-  useUserStore.setState({ userData: null });
+  useUserStore.setState({ userData: USER_INITIAL_DATA });
 };
