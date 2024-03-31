d49ae4a4422be09a1bf8fc8ec8d1ff9797eaea18
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 02:29:45 +0900

    refactor: useUserStore의 useData에 null이 들어가지 않도록 타입과 초기값 수정

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -1,14 +1,16 @@
 import { create } from 'zustand';
 
+import { USER_INITIAL_DATA } from '@/constants';
+
 import { UsersResponse } from '@/types';
 
 type UserStoreState = {
-  userData: UsersResponse | null;
+  userData: UsersResponse;
   setUserData: (newData: UsersResponse) => void;
 };
 
 const useUserStore = create<UserStoreState>((set) => ({
-  userData: null,
+  userData: USER_INITIAL_DATA,
   setUserData: (newData: UsersResponse) => set({ userData: newData }),
 }));
 
