504fe09ce6993ecce320b91927c08f5d4f4ca120
designsoo <kheesoo10@gmail.com>
2024-03-31 18:21:09 +0900

    feat: 랜딩페이지 SSR 적용 및 리액트 쿼리로 게임 리스트 props 전달

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,7 +1,33 @@
+import { dehydrate } from '@tanstack/react-query';
+
+import { getActivities } from '@/apis/queryFunctions';
+import { queryClient } from '@/utils';
+
+import ClanRecruitment from '@/components/landing/ClanRecruitment';
 import Layout from '@/components/layout/Layout';
 
+import { Category } from '@/types';
+
+export const getServerSideProps = () => {
+  queryClient.prefetchQuery({ queryKey: ['activities', ''], queryFn: getActivities });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export type LandingPageProps = {
+  category: Category;
+};
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <ClanRecruitment />
+    </div>
+  );
 };
 
 export default LandingPage;
