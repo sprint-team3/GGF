f512592a0a2b452634d601747b4e94d9da7175d1
CheeseB <2489ckckck@naver.com>
2024-03-31 19:19:11 +0900

    feat: 화면 양옆에 흐르는 텍스트 추가

--- src/components/landing/Echo/index.tsx
+++ src/components/landing/Echo/index.tsx
@@ -1,5 +1,7 @@
 import classNames from 'classnames/bind';
 
+import { GAME_NAME_LIST_EN } from '@/constants';
+
 import useMouseMoveEffect from '@/hooks/useMouseMoveEffect';
 
 import styles from './Echo.module.scss';
@@ -12,6 +14,26 @@ const Echo = () => {
     <section ref={containerRef} className={cx('container')}>
       <div ref={elementRef} className={cx('echo-shadow')}></div>
       <div ref={elementRef} className={cx('echo')}></div>
+      <div className={cx('side-scroll', 'left')}>
+        <ul className={cx('scroll-content', 'left')}>
+          {GAME_NAME_LIST_EN.map((gameName, index) => (
+            <li key={`game-left-${index}`}>{gameName}</li>
+          ))}
+          {GAME_NAME_LIST_EN.map((gameName, index) => (
+            <li key={`game-left-clone-${index}`}>{gameName}</li>
+          ))}
+        </ul>
+      </div>
+      <div className={cx('side-scroll', 'right')}>
+        <ul className={cx('scroll-content', 'right')}>
+          {GAME_NAME_LIST_EN.map((gameName, index) => (
+            <li key={`game-right-${index}`}>{gameName}</li>
+          ))}
+          {GAME_NAME_LIST_EN.map((gameName, index) => (
+            <li key={`game-right-clone-${index}`}>{gameName}</li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 };
