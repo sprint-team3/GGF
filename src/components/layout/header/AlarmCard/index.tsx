3f2d3f04222359ad50921f6702d40701f9abb01b
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 00:44:06 +0900

    feat: 알람 개별 삭제 api 및 경고 모달 연결

--- src/components/layout/header/AlarmCard/index.tsx
+++ src/components/layout/header/AlarmCard/index.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image';
 
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import classNames from 'classnames/bind';
 
-import { MyNotifications } from '@/apis/myNotifications';
-import { SVGS } from '@/constants';
+import { deleteMyNotification } from '@/apis/queryFunctions';
+import { QUERY_KEYS } from '@/apis/queryKeys';
+import { API_ERROR_MESSAGE, SVGS } from '@/constants';
 import { getElapsedTimeToKST } from '@/utils';
 
+import { ConfirmModal, ModalButton } from '@/components/commons/modals';
 import useToggleButton from '@/hooks/useToggleButton';
 
 import styles from './AlarmCard.module.scss';
@@ -21,31 +25,59 @@ type AlarmCardProps = {
 
 const AlarmCard = ({ id, content, createdAt }: AlarmCardProps) => {
   const { isVisible: hoverState, handleToggleClick: handleToggleState } = useToggleButton();
+  const { isVisible: is404Open, handleToggleClick: handle404Click } = useToggleButton();
+
+  const queryClient = useQueryClient();
+
+  const { mutate: deleteAlarmMutation } = useMutation({
+    mutationFn: deleteMyNotification,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.myNotifications.get],
+      });
+    },
+    onError: (error) => {
+      if ((error as AxiosError)?.response?.status === 404) {
+        handle404Click();
+      }
+    },
+  });
 
   const handleDeleteNotification = (id: number) => {
-    MyNotifications.delete(id);
+    deleteAlarmMutation(id);
   };
 
   return (
-    <div className={cx('alarm-card')}>
-      <div className={cx('alarm-card-container')}>
-        <p className={cx('alarm-card-content')}>{content}</p>
-        <button
-          className={cx('alarm-card-delete')}
-          onMouseEnter={handleToggleState}
-          onMouseLeave={handleToggleState}
-          onClick={() => handleDeleteNotification(id)}
-        >
-          <Image
-            src={hoverState ? hover.url : normal.url}
-            alt={hoverState ? hover.alt : normal.alt}
-            width={24}
-            height={24}
-          />
-        </button>
+    <>
+      <div className={cx('alarm-card')}>
+        <div className={cx('alarm-card-container')}>
+          <p className={cx('alarm-card-content')}>{content}</p>
+          <button
+            className={cx('alarm-card-delete')}
+            onMouseEnter={handleToggleState}
+            onMouseLeave={handleToggleState}
+            onClick={() => handleDeleteNotification(id)}
+          >
+            <Image
+              src={hoverState ? hover.url : normal.url}
+              alt={hoverState ? hover.alt : normal.alt}
+              width={24}
+              height={24}
+            />
+          </button>
+        </div>
+        <span className={cx('alarm-card-created-at')}>{getElapsedTimeToKST(createdAt)}</span>
       </div>
-      <span className={cx('alarm-card-created-at')}>{getElapsedTimeToKST(createdAt)}</span>
-    </div>
+      <ConfirmModal
+        warning
+        openModal={is404Open}
+        onClose={() => handle404Click}
+        state='ERROR'
+        title={'알림 삭제에 실패하였습니다'}
+        desc={API_ERROR_MESSAGE.notification[404]}
+        renderButton={<ModalButton onClick={() => handle404Click}>닫기</ModalButton>}
+      ></ConfirmModal>
+    </>
   );
 };
 
