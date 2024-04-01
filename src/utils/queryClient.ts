ffc74e110eb4c6c980aad710e27657fbc9a7fda3
ccwnc <ccwnc337@gmail.com>
2024-04-01 18:04:38 +0900

    refactor: constants의 queryClient 파일 삭제 및 staleTime 제거

--- src/utils/queryClient.ts
+++ src/utils/queryClient.ts
@@ -1,11 +1,8 @@
 import { QueryClient } from '@tanstack/react-query';
 
-import { QUERY_CLIENT_STALE_TIME } from '@/constants';
-
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: QUERY_CLIENT_STALE_TIME,
       refetchOnWindowFocus: false,
       retry: 0,
     },
