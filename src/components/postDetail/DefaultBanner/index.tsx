da96b0e2276e6f53b743e47a7e8fb767114e0e0a
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 15:52:45 +0900

    refactor: DefaultBanner price값으로 모집 유형가져오도록 수정 및 마크업 수정

--- src/components/postDetail/DefaultBanner/index.tsx
+++ src/components/postDetail/DefaultBanner/index.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 
 import classNames from 'classnames/bind';
 
+import { PRICE_TO_MATCH_TYPE } from '@/constants';
+
 import { GameNameEN } from '@/types';
 
 import styles from './DefaultBanner.module.scss';
@@ -10,15 +12,19 @@ const cx = classNames.bind(styles);
 
 type DefaultBannerProps = {
   url: string;
+  price: number;
   gameName: GameNameEN;
 };
 
-const DefaultBanner = ({ url, gameName }: DefaultBannerProps) => {
+const DefaultBanner = ({ url, price, gameName }: DefaultBannerProps) => {
   return (
     <div className={cx('default-banner')}>
       <div className={cx('img-container')}>
         <Image className={cx('img')} src={url} alt='배너 이미지' fill sizes='100%' />
-        <span className={cx('game-name')}>{gameName}</span>
+        <div className={cx('text-container')}>
+          <span className={cx('match-name')}>{PRICE_TO_MATCH_TYPE[price]}</span>
+          <span className={cx('game-name')}>{gameName}</span>
+        </div>
       </div>
     </div>
   );
