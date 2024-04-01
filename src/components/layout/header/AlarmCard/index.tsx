99d2b1fc34458bfe865ad511e23ba0a360550556
grapefruit13 <liam.heo11@gmail.com>
2024-04-02 00:27:23 +0900

    refactor: 알림 메시지 가공

--- src/components/layout/header/AlarmCard/index.tsx
+++ src/components/layout/header/AlarmCard/index.tsx
@@ -7,7 +7,7 @@ import classNames from 'classnames/bind';
 import { deleteMyNotification } from '@/apis/queryFunctions';
 import { QUERY_KEYS } from '@/apis/queryKeys';
 import { API_ERROR_MESSAGE, SVGS } from '@/constants';
-import { getElapsedTimeToKST } from '@/utils';
+import { getElapsedTimeToKST, splitTitleByDelimiter } from '@/utils';
 
 import { ConfirmModal, ModalButton } from '@/components/commons/modals';
 import useToggleButton from '@/hooks/useToggleButton';
@@ -26,6 +26,9 @@ type AlarmCardProps = {
 const AlarmCard = ({ id, content, createdAt }: AlarmCardProps) => {
   const { isVisible: hoverState, handleToggleClick: handleToggleState } = useToggleButton();
   const { isVisible: is404Open, handleToggleClick: handle404Click } = useToggleButton();
+
+  const { title, MaxCount } = splitTitleByDelimiter(content);
+  const refinedContent = title + (MaxCount.match(/\(.*$/)?.[0] ?? '');
 
   const queryClient = useQueryClient();
 
@@ -51,7 +54,7 @@ const AlarmCard = ({ id, content, createdAt }: AlarmCardProps) => {
     <>
       <div className={cx('alarm-card')}>
         <div className={cx('alarm-card-container')}>
-          <p className={cx('alarm-card-content')}>{content}</p>
+          <p className={cx('alarm-card-content')}>{refinedContent}</p>
           <button
             className={cx('alarm-card-delete')}
             onMouseEnter={handleToggleState}
@@ -71,11 +74,11 @@ const AlarmCard = ({ id, content, createdAt }: AlarmCardProps) => {
       <ConfirmModal
         warning
         openModal={is404Open}
-        onClose={() => handle404Click}
+        onClose={handle404Click}
         state='ERROR'
         title={'알림 삭제에 실패하였습니다'}
         desc={API_ERROR_MESSAGE.notification[404]}
-        renderButton={<ModalButton onClick={() => handle404Click}>닫기</ModalButton>}
+        renderButton={<ModalButton onClick={handle404Click}>닫기</ModalButton>}
       ></ConfirmModal>
     </>
   );
