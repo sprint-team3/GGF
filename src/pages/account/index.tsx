214ee899fc13d2e71ebd6cdccc5be6456bdea6e3
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 13:42:38 +0900

    refactor: requriesLogin 사용하는 account와 mypage 수정

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,19 +1,13 @@
 import { GetServerSidePropsContext } from 'next';
 
 import { PAGE_PATHS } from '@/constants';
-import { getAuthCookie, requiresLogin, setAuthCookie } from '@/utils';
+import { requiresLogin } from '@/utils';
 
 import AccountForm from '@/components/AccountForm';
 import Layout from '@/components/layout/Layout';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { accessToken, refreshToken } = getAuthCookie(context);
-
-  const res = await requiresLogin(context, accessToken, refreshToken, PAGE_PATHS.mainList);
-  if (res) {
-    const { newAccessToken, newRefreshToken } = res;
-    setAuthCookie(context, newAccessToken, newRefreshToken);
-  }
+  await requiresLogin(context, PAGE_PATHS.signin);
 
   return {
     props: {},
