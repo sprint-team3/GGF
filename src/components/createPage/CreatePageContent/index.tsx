27b05d5e8376e52bec5dca1d9c812d9d9283f20e
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 14:03:01 +0900

    refactor: Banner 수정에 따른 create page prop 수정

--- src/components/createPage/CreatePageContent/index.tsx
+++ src/components/createPage/CreatePageContent/index.tsx
@@ -3,10 +3,10 @@ import { CreatePageContentProps } from '@/pages/[game]/create';
 import PostForm from '@/components/createPage/PostForm';
 import Banner from '@/components/layout/Banner';
 
-const CreatePageContent = ({ gameName, category }: CreatePageContentProps) => {
+const CreatePageContent = ({ category }: CreatePageContentProps) => {
   return (
     <>
-      <Banner gameName={gameName} />
+      <Banner />
       <PostForm type='등록' category={category} />
     </>
   );
