522383840ff2bd2648e0924b225d494ae6935f03
grapefruit13 <liam.heo11@gmail.com>
2024-04-01 13:42:38 +0900

    refactor: requriesLogin 사용하는 account와 mypage 수정

--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -5,19 +5,13 @@ import { dehydrate } from '@tanstack/react-query';
 import { getMyActivitiesList, getMyReservations, getUser } from '@/apis/queryFunctions';
 import { QUERY_KEYS } from '@/apis/queryKeys';
 import { PAGE_PATHS } from '@/constants';
-import { queryClient, getAuthCookie, requiresLogin, setAuthCookie } from '@/utils';
+import { queryClient, requiresLogin } from '@/utils';
 
 import Layout from '@/components/layout/Layout';
 import MypageContent from '@/components/mypage/MypageContent';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const { accessToken, refreshToken } = getAuthCookie(context);
-
-  const res = await requiresLogin(context, accessToken, refreshToken, PAGE_PATHS.landing);
-  if (res) {
-    const { newAccessToken, newRefreshToken } = res;
-    setAuthCookie(context, newAccessToken, newRefreshToken);
-  }
+  await requiresLogin(context, PAGE_PATHS.landing);
 
   await queryClient.prefetchQuery({ queryKey: QUERY_KEYS.myActivities.getList, queryFn: getMyActivitiesList });
 
