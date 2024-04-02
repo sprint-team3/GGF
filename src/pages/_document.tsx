22462c710c37ad95895111b692fc53026040a219
grapefruit13 <liam.heo11@gmail.com>
2024-04-03 01:44:09 +0900

    feat: head 공통 메타태그 document에 등록

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,37 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
+import { DEFAULT_META_DATA } from '@/constants';
+
 export default function Document() {
   return (
     <Html lang='ko'>
-      <Head />
+      <Head>
+        <link rel='canonical' href={DEFAULT_META_DATA.siteUrl} key='canonical' />
+        <title>{DEFAULT_META_DATA.title}</title>
+        <meta name='description' content={DEFAULT_META_DATA.description} />
+        <meta name='keywords' content={DEFAULT_META_DATA.keywords} />
+        <meta name='robots' content='index, follow' />
+        <meta property='og:type' content='website' />
+        <meta property='og:site_name' content='GGF' />
+        <meta property='og:title' content={DEFAULT_META_DATA.title} />
+        <meta property='og:description' content={DEFAULT_META_DATA.ogDescription} />
+        <meta property='og:locale' content='ko_KR' />
+        <meta property='og:url' content={DEFAULT_META_DATA.siteUrl} />
+        <meta property='og:image' content={DEFAULT_META_DATA.imageUrl} />
+        <meta property='og:image:secure_url' content={DEFAULT_META_DATA.imageUrl} />
+        <meta property='og:image:type' content='image/png' />
+        <meta property='og:image:width' content={DEFAULT_META_DATA.ogImageWidth} />
+        <meta property='og:image:height' content={DEFAULT_META_DATA.ogImageHeight} />
+        <meta property='og:image:alt' content={DEFAULT_META_DATA.ogImageAlt} />
+
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content={DEFAULT_META_DATA.siteUrl} />
+        <meta name='twitter:title' content={DEFAULT_META_DATA.title} />
+        <meta name='twitter:description' content={DEFAULT_META_DATA.ogDescription} />
+        <meta name='twitter:image' content={DEFAULT_META_DATA.twitterImageUrl} />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
