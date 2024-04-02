c8a43c090b0c57cae04a71b418233eabf38e943d
grapefruit13 <liam.heo11@gmail.com>
2024-04-03 01:48:17 +0900

    feat: 메타태그 객체에 저장

---
+++ src/constants/metaData.ts
@@ -0,0 +1,23 @@
+export const DEFAULT_META_DATA = {
+  title: 'GGF | Good Game Friends',
+  description:
+    '게임을 사랑하는 이들을 위한 특별한 공간으로, 다양한 게임을 즐기며 함께하는 멋진 친구들을 만날 수 있는 커뮤니티 웹사이트입니다.',
+  keywords: 'game, matchGame, lol, battlegrounds, overwatch, minecraft',
+  ogDescription: '게임을 사랑하는 이들을 위한 특별한 공간 GGF',
+  ogImageWidth: '800',
+  ogImageHeight: '400',
+  ogImageAlt: 'GGF 로고와 qwer',
+  siteUrl: 'https://good-game-friends.vercel.app/landing',
+  imageUrl:
+    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/profile_image/2-3_116_1712073356012.png',
+  twitterImageUrl:
+    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/globalnomad/profile_image/2-3_116_1712073189802.png',
+};
+
+export const META_DATA = {
+  signin: {
+    title: '로그인',
+    description: 'GGF 로그인 페이지 입니다.',
+    keywords: '로그인, 인증, 로그인 페이지',
+  },
+};
