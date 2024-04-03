b4b8e40d22f9ac9bdea528d5c18617a43cfdce1d
ccwnc <ccwnc337@gmail.com>
2024-04-03 21:51:40 +0900

    refactor: 모집 유형을 확인하는 유틸 함수 추가

--- src/utils/isValidGameName.ts
+++ src/utils/isValidGameName.ts
@@ -1,5 +1,17 @@
+import { POST_TYPES_FOR_LISTPAGE } from '@/constants';
+
 import { LinkName } from '@/types';
 
 export const isValidGameName = (game: string): game is LinkName => {
   return ['league-of-legends', 'battlegrounds', 'overwatch-2', 'minecraft'].includes(game);
+};
+
+export const isValidPostType = (query: string) => {
+  const validPostTypes = String(POST_TYPES_FOR_LISTPAGE.map((item) => item.id));
+
+  if (!validPostTypes.includes(query)) {
+    return false;
+  }
+
+  return true;
 };
