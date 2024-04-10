9242b03e31f45140f5402bbc60c75b668130347a
designsoo <kheesoo10@gmail.com>
2024-04-10 23:03:40 +0900

    feat: 챗봇 타입 추가

---
+++ src/types/chatbot.ts
@@ -0,0 +1,12 @@
+export type Chat = {
+  question: string;
+  answer: string;
+  questionDate: string;
+  answerDate: string;
+};
+
+export type FaqChat = {
+  id: number;
+  title: string;
+  description: string;
+};
