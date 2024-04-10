5689b42874ee4ddd28042b5accc39ee55b69c9bc
designsoo <kheesoo10@gmail.com>
2024-04-10 23:14:13 +0900

    refactor: faq 전체 다이어로그를 포함하는 컴포넌트로 변경

--- src/components/commons/chatbot/FAQChatDialog.tsx
+++ src/components/commons/chatbot/FAQChatDialog.tsx
@@ -4,42 +4,55 @@ import classNames from 'classnames/bind';
 
 import { FAQ } from '@/components/commons/chatbot/FAQ';
 
+import { FaqChat } from '@/types';
+
 import styles from './FAQChatDialog.module.scss';
 
 const cx = classNames.bind(styles);
 
 type FAQChatDialogProps = {
-  title: string;
-  description: string;
+  faqChat: FaqChat[];
   onClick: (action: number) => void;
 };
 
-export const FAQChatDialog = ({ title, description, onClick }: FAQChatDialogProps) => {
-  const [isOpen, setIsOpen] = useState(false);
+export const FAQChatDialog = ({ faqChat, onClick }: FAQChatDialogProps) => {
+  const faqOpenList = Array.from({ length: faqChat.length }, () => false);
 
-  const handleFaqOpen = () => {
-    setIsOpen(true);
+  const [openFaq, setOpenFaq] = useState(faqOpenList);
+
+  const handleFaqOpen = (index: number) => {
+    setOpenFaq((prev) => {
+      const updatedOpenFAQ = [...prev];
+      updatedOpenFAQ[index] = true;
+      return updatedOpenFAQ;
+    });
   };
 
   return (
-    <ul className={cx('faq-chat-list')}>
-      <li className={cx('faq-chat-list-question')}>
-        <p className={cx('faq-chat-list-question-description')}>{title}</p>
-      </li>
-      <li className={cx('faq-chat-list-answer')}>
-        <p className={cx('faq-chat-list-answer-description')}>
-          {description.split('\n').map((answer, index) => (
-            <Fragment key={`answer-${index}`}>
-              {answer}
-              <br />
-            </Fragment>
-          ))}
-        </p>
-      </li>
-      <button className={cx('faq-visibility-button')} onClick={handleFaqOpen}>
-        FAQ 불러오기
-      </button>
-      {isOpen && <FAQ onClick={onClick} />}
-    </ul>
+    <div>
+      {faqChat.map((faq, index) => (
+        <div key={`faq-${index}`} className={cx('faq-chat-dialog-list')}>
+          <ul className={cx('faq-chat-list')}>
+            <li className={cx('faq-chat-list-question')}>
+              <p className={cx('faq-chat-list-question-description')}>{faq.title}</p>
+            </li>
+            <li className={cx('faq-chat-list-answer')}>
+              <p className={cx('faq-chat-list-answer-description')}>
+                {faq.description.split('\n').map((answer, index) => (
+                  <Fragment key={`answer-${index}`}>
+                    {answer}
+                    <br />
+                  </Fragment>
+                ))}
+              </p>
+            </li>
+            <button className={cx('faq-visibility-button')} onClick={() => handleFaqOpen(index)}>
+              FAQ 불러오기
+            </button>
+            {openFaq[index] && <FAQ onClick={onClick} />}
+          </ul>
+        </div>
+      ))}
+    </div>
   );
 };
